164265fa70c0f279da769d39d155855fe54fbd34
Brian Adkins <info@lojic.com>
2016-02-27 01:03:53 -0500

    add 2nd version of binary ex.

---
+++ exercism.io/binary/binary-a.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(provide to-decimal)
+
+(define (to-decimal str)
+  (let loop ([lst (string->list str)] [acc 0])
+    (cond [ (null? lst) acc ]
+          [ (char=? #\0 (car lst)) (loop (cdr lst) (* 2 acc)) ]
+          [ (char=? #\1 (car lst)) (loop (cdr lst) (+ (* 2 acc) 1)) ]
+          [ else 0 ])))
+          
