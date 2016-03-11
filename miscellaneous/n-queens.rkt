357d4d295e040b5f3334f980741f8c2d7dbbe508
Brian Adkins <info@lojic.com>
2016-03-10 22:28:28 -0500

    Compute all solutions instead of just the first

--- miscellaneous/n-queens.rkt
+++ miscellaneous/n-queens.rkt
@@ -1,31 +1,49 @@
 #lang racket
 
 ; Represent a position as a struct with file and rank members
-(struct pos (file rank) #:transparent)
+(struct pos (x y) #:transparent)
 
 ; Indicate whether q1 is attacking q2
 (define (is-attacking? q1 q2)
-  (or (= (pos-rank q1) (pos-rank q2))
-      (= (abs (- (pos-rank q1) (pos-rank q2)))
-         (abs (- (pos-file q1) (pos-file q2))))))
+  (or (= (pos-y q1) (pos-y q2))
+      (= (abs (- (pos-y q1) (pos-y q2)))
+         (abs (- (pos-x q1) (pos-x q2))))))
 
 ; Indicate whether the stack of positions is valid
 (define (valid? stack)
   (not (ormap (curry is-attacking? (car stack)) (cdr stack))))
 
-; Return a list of positions where no queen is attacking another queen
-(define (queens n)
-  (let loop ([result (list (pos 1 1))])
-    (match-define (pos file rank) (car result))
-    (cond
-      [(> file n) (reverse (cdr result))] ; Return solution
-      [(valid? result) (loop (cons (pos (+ file 1) 1) result))] ; Go to next file
-      [(< rank n) (loop (cons (pos file (+ rank 1)) (cdr result)))] ; Go to next rank
-      [else (let inner ([result result]) ; Backtrack
-              (let ([new-rank (+ (pos-rank (cadr result)) 1)])
-                (if (<= new-rank n)
-                    (loop (cons (pos (- (pos-file (car result)) 1) new-rank)
-                                (cddr result)))
-                    (inner (cdr result)))))])))
+; Return a stack representing the next position, or #f if none exist
+(define (next-position n stack)
+  (cond [(null? stack) #f]
+        [ else (match-define (pos x y) (car stack))
+               (if (< y n)
+                   (cons (pos x (+ y 1)) (cdr stack))
+                   (next-position n (cdr stack)))]))
 
-(queens 8) ; => (list (pos 1 1) (pos 2 5) (pos 3 8) (pos 4 6) (pos 5 3) (pos 6 7) (pos 7 2) (pos 8 4))
+; Accept a board size and partial list of moves, and return the next solution
+(define (queens n stack)
+  (let loop ([stack stack])
+    (match-define (pos file rank) (car stack))
+    (cond
+      [(> file n) (cdr stack)] ; Return solution
+      [(valid? stack) (loop (cons (pos (+ file 1) 1) stack))] ; Go to next file
+      [(< rank n) (loop (cons (pos file (+ rank 1)) (cdr stack)))] ; Go to next rank
+      [else (let ([next (next-position n (cdr stack))]) ;Backtrack
+              (if next
+                  (loop next)
+                  #f))])))
+
+; Return a list of all solutions for the specified board size
+(define (main n)
+  (let loop ([stack (list (pos 1 1))]
+             [solutions '()])
+    (if stack
+        (let ([solution (queens n stack)])
+          (if solution
+              (loop (next-position n solution)
+                    (cons solution solutions))
+              solutions))
+        solutions)))
+
+(time (length (main 13)))
