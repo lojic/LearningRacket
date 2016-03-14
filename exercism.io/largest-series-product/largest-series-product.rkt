949b711f104635c762e5ca82b94c732264f034e9
Brian Adkins <info@lojic.com>
2016-03-14 12:40:31 -0400

    refactored to one function

--- exercism.io/largest-series-product/largest-series-product.rkt
+++ exercism.io/largest-series-product/largest-series-product.rkt
@@ -2,16 +2,12 @@
 (provide largest-series-product)
 
 (define (largest-series-product str n)
-  (define digits (string->digits str))
-  (define max-count (+ (- (length digits) n) 1))
-  (let loop ([lst digits] [largest-product 0] [count 0])
-    (if (< count max-count)
-        (loop (cdr lst) (max largest-product (series-product lst n)) (+ count 1))
-        largest-product)))
-
-(define (string->digits str)
-  (map (λ (c) (- (char->integer c) (char->integer #\0)))
-       (string->list str)))
-
-(define (series-product lst n)
-  (apply * (take lst n)))
+  (let* ([digits (for/list ([c (in-string str)])
+                           (- (char->integer c) (char->integer #\0)))]
+         [max-count (+ (- (length digits) n) 1)]
+         [series-product (λ (lst n) (apply * (take lst n)))])
+    (let loop ([lst digits] [largest-product 0] [count 0])
+      (if (< count max-count)
+          (loop (cdr lst) (max largest-product (series-product lst n)) (+ count 1))
+          largest-product))))
+  
