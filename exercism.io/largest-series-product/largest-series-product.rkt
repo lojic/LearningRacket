04f77d5546b88107def76e1938e8bbde420be940
Brian Adkins <info@lojic.com>
2016-03-14 12:51:17 -0400

    refactor to not allocate intermed. lists

--- exercism.io/largest-series-product/largest-series-product.rkt
+++ exercism.io/largest-series-product/largest-series-product.rkt
@@ -4,9 +4,17 @@
 (define (largest-series-product str n)
   (let* ([digits (for/list ([c (in-string str)])
                            (- (char->integer c) (char->integer #\0)))]
-         [max-count (+ (- (length digits) n) 1)]
-         [series-product (λ (lst n) (apply * (take lst n)))])
+         [max-count (+ (- (length digits) n) 1)])
     (let loop ([lst digits] [largest-product 0] [count 0])
       (if (< count max-count)
-          (loop (cdr lst) (max largest-product (series-product lst n)) (+ count 1))
+          (loop (cdr lst)
+                (max largest-product
+                     (series-product lst n 1))
+                (+ count 1))
           largest-product))))
+
+(define (series-product lst n acc)
+  (if (> n 0)
+      (series-product (cdr lst) (- n 1) (* (car lst) acc))
+      acc))
+      
