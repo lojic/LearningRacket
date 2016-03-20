50f4a4f40427020a203490dabd9fb394031679f1
Brian Adkins <info@lojic.com>
2016-03-20 00:06:57 -0400

    formatting

--- exercism.io/pythagorean-triplet/pythagorean-triplet.rkt
+++ exercism.io/pythagorean-triplet/pythagorean-triplet.rkt
@@ -1,19 +1,19 @@
 #lang racket
 (provide (all-defined-out))
 
-(define (sum tuple)
-  (apply + tuple))
+(define (sum tup)
+  (apply + tup))
 
-(define (product tuple)
-  (apply * tuple))
+(define (product tup)
+  (apply * tup))
 
 (define (pythagorean? a b c)
   (= (+ (* a a) (* b b)) (* c c)))
 
 (define (generate beg end [sum 0])
-  (for*/list ([a (in-range beg end)    ]
-              [b (in-range (+ a 1) end)]
-              [c (in-range (+ b 1) end)]
+  (for*/list ([a (in-range beg end)]
+              [b (in-range a end)]
+              [c (in-range b end)]
               #:when (and (or (= sum 0) (= sum (+ a b c)))
                           (pythagorean? a b c)))
              (list a b c)))
