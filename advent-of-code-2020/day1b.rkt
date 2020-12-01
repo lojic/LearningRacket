8d3452bc57743ab1deb3f9529ad56a36819da256
Brian Adkins <brian@lojic.com>
2020-12-01 17:53:25 -0500

    Better, more general permutation function

--- advent-of-code-2020/day1b.rkt
+++ advent-of-code-2020/day1b.rkt
@@ -3,6 +3,10 @@
 ;; Advent of Code 2020 Day 1 part 2:
 ;; Find three numbers in the input that sum to 2020, then multiply
 ;; them to obtain the puzzle answer.
+;;
+;; This solution is quite a bit more general than what is required. I
+;; had fun coming up with the filter-ascending-permutations function,
+;; and I think I may use it in the future.
 
 (define expense-sum       2020)
 (define expenses-filename "day1.txt")
@@ -25,30 +29,33 @@
 ;; whether they sum to the specified value.
 (define (sums-to n) (λ (lst) (= n (apply + lst))))
 
-;; (find-n pred? n lst result) -> (or/c list? #f)
+;; (filter-ascending-permutations pred? n lst) -> list?
 ;; pred?  : procedure?
 ;; n      : exact-nonnegative-integer?
 ;; lst    : (listof number?)
-;; result : (listof number?)
 ;;
-;; Searches for n distinct numbers in a list that satisfy the
-;; specified predicate. If found, returns (list n1 n2 ... nn);
-;; otherwise, returns #f
-(define (find-n pred? n lst [ result '() ])
-  (if (= n 0)
-      (if (pred? result)
-          (reverse result)
-          #f)
-      (if (null? lst)
-          #f
-          (let* ([ num (car lst) ]
-                 [ ans (find-n pred? (sub1 n) (cdr lst) (cons num result)) ])
-            (if ans
-                ans
-                (find-n pred? n (cdr lst) result))))))
+;; Return a list of permutations that satisfy the specified
+;; predicate. For example, the list '(foo bar foo baz) produces the
+;; following list of 2-tuple ascending permutations:
+;; '((foo bar) (foo foo) (foo baz) (bar foo) (bar baz) (foo baz))
+(define (filter-ascending-permutations pred? n lst)
+  (reverse
+   (let loop ([ lst lst ][ n n ][ stack '() ][ result '() ])
+     (if (= n 0)
+         (let ([ s (reverse stack) ])
+           (if (pred? s) (cons s result) result))
+         (if (null? lst)
+             result
+             (loop (cdr lst)
+                   n
+                   stack
+                   (loop (cdr lst) (sub1 n) (cons (car lst) stack) result)))))))
 
 (define (find-3 pred? lst)
-  (find-n pred? 3 lst))
+  (let ([ tuple (filter-ascending-permutations pred? 3 lst) ])
+    (if (null? tuple)
+        #f
+        (first tuple))))
 
 (module+ main
   (let ([ numbers (find-3 (sums-to expense-sum)
@@ -88,9 +95,19 @@
   (check-equal? (find-3 (sums-to 4) '(2 1 7 4 0 1)) '(2 1 1))
   
   ;; ------------------------------------------------------------------------------------------
-  ;; find-n
+  ;; filter-ascending-permutations
   ;; ------------------------------------------------------------------------------------------
-  (check-equal? (find-n (λ (lst) (andmap even? lst)) 3 '(1 2 3 4 5 6)) '(2 4 6))
-  (check-equal? (find-n (sums-to 17) 2 '(1 2 3 4 5 6 7 8 9 10)) '(7 10))
+  (check-equal? (filter-ascending-permutations identity 2 '(foo bar foo baz))
+                '((foo bar) (foo foo) (foo baz) (bar foo) (bar baz) (foo baz)))
+  
+  (check-equal? (filter-ascending-permutations (sums-to 7) 2 '(1 2 3 4))
+                '((3 4)))
+  
+  ;; ------------------------------------------------------------------------------------------
+  ;; Solution
+  ;; ------------------------------------------------------------------------------------------
+  (check-equal? (apply * (find-3 (sums-to expense-sum)
+                                 (get-expenses expenses-filename)))
+                178724430)
   
   )
