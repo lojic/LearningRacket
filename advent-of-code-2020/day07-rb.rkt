a99348805451a1bbe222faa35465d414f28a1071
Brian Adkins <brian@lojic.com>
2020-12-07 20:22:33 -0500

    Port of Ruby version

---
+++ advent-of-code-2020/day07-rb.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+;; Version inspired by a Ruby version I read.
+
+(require threading)
+
+(define (part1 hsh bag)
+  (~> (hash->list hsh)
+      (filter (λ (l) (member bag (cdr l))) _)
+      (map (λ (l) (part1 hsh (car l))) _)
+      (foldl (λ (l result) (set-union result l)) (set bag) _)))
+
+(~> (file->lines "day07.txt")
+    (map (curry regexp-match* #px"([a-z]+ [a-z]+)(?= bag)") _)
+    (make-immutable-hasheq _)
+    (part1 _ "shiny gold")
+    (set-count _)
+    (sub1 _))
+
+
