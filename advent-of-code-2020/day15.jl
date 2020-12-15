a73578ae2b48d790bbf4c0a9eecea8ca30640942
Brian Adkins <brian@lojic.com>
2020-12-15 16:58:51 -0500

    Add Julia version

---
+++ advent-of-code-2020/day15.jl
@@ -0,0 +1,25 @@
+function run(numbers, limit::Int64)
+  vec = zeros(Int32, 30000000)
+
+  turn = 1
+  for num in numbers[1:end-1]
+    vec[num + 1] = turn
+    turn += 1
+  end
+
+  turn::Int32, last::Int32 = (length(numbers), numbers[end])
+
+  while turn < limit
+    idx       = last + 1 # Julia's arrays are one based
+    prev_turn = vec[idx]
+    vec[idx]  = turn
+    last      = (prev_turn > 0) ? turn - prev_turn : 0
+    turn     += 1
+  end
+
+  last
+end
+
+@show run([12,20,0,6,1,17,7], 2020)
+@show run([12,20,0,6,1,17,7], 30000000)
+@time run([12,20,0,6,1,17,7], 30000000)
