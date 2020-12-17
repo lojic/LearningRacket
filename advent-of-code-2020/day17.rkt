ee754e3159bb5743362c02914b43fbce5609b4d1
Brian Adkins <brian@lojic.com>
2020-12-17 14:25:21 -0500

    Factored out common code

--- advent-of-code-2020/day17.rkt
+++ advent-of-code-2020/day17.rkt
@@ -21,36 +21,21 @@
 
 (define (triple n) (list (- n 1) n (+ n 1)))
 
-(define (part1 hsh x-dim y-dim z-dim w-dim)
-  (define (active-neighbors hsh x0 y0 z0 _)
-    (for*/sum ([ x (triple x0) ][ y (triple y0) ][ z (triple z0) ]
-               #:when (and (not (and (= x0 x) (= y0 y) (= z0 z)))
-                           (hash-ref hsh (key x y z) #f))) 1))
+(define (run 4d?)
+  (match-let ([ (list hsh x-dim y-dim z-dim w-dim) (parse "day17.txt") ])
+    (define (active-neighbors hsh x0 y0 z0 w0)
+      (for*/sum ([ x (triple x0) ] [ y (triple y0) ] [ z (triple z0) ][ w (if 4d? (triple w0) (in-range 1)) ]
+                 #:when (and (not (and (= x0 x) (= y0 y) (= z0 z) (= w0 w)))
+                             (hash-ref hsh (key x y z w) #f))) 1))
 
-  (λ (hsh)
-    (for*/hash ([ x (in-range (- cycles) (+ x-dim cycles 1)) ]
-                [ y (in-range (- cycles) (+ y-dim cycles 1)) ]
-                [ z (in-range (- cycles) (+ cycles 1))       ]
-                #:when (set-active? hsh (key x y z) active-neighbors))
-      (values (key x y z) #t))))
+    (define (cycle hsh)
+      (for*/hash ([ x (in-range (- cycles) (+ x-dim cycles 1)) ]
+                  [ y (in-range (- cycles) (+ y-dim cycles 1)) ]
+                  [ z (in-range (- cycles) (+ cycles 1))       ]
+                  [ w (if 4d? (in-range (- cycles) (+ cycles 1)) (in-range 1)) ]
+                  #:when (set-active? hsh (key x y z w) active-neighbors))
+        (values (key x y z w) #t)))
+  (hash-count (iterate cycle hsh 6))))
 
-(define (part2 hsh x-dim y-dim z-dim w-dim)
-  (define (active-neighbors hsh x0 y0 z0 w0)
-    (for*/sum ([ x (triple x0) ] [ y (triple y0) ] [ z (triple z0) ] [ w (triple w0) ]
-               #:when (and (not (and (= x0 x) (= y0 y) (= z0 z) (= w0 w)))
-                           (hash-ref hsh (key x y z w) #f))) 1))
-
-  (λ (hsh)
-    (for*/hash ([ x (in-range (- cycles) (+ x-dim cycles 1)) ]
-                [ y (in-range (- cycles) (+ y-dim cycles 1)) ]
-                [ z (in-range (- cycles) (+ cycles 1))       ]
-                [ w (in-range (- cycles) (+ cycles 1))       ]
-                #:when (set-active? hsh (key x y z w) active-neighbors))
-      (values (key x y z w) #t))))
-
-(define (run part)
-  (let ([ lst (parse "day17.txt") ])
-    (hash-count (iterate (apply part lst) (car lst) 6))))
-
-(check-equal? (run part1) 232)
-(check-equal? (run part2) 1620)
+(check-equal? (run #f) 232)
+(check-equal? (run #t) 1620)
