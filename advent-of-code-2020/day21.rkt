3629f423bd2456435ee51ae6f16364443e46b740
Brian Adkins <brian@lojic.com>
2020-12-21 16:50:33 -0500

    last tweak?

--- advent-of-code-2020/day21.rkt
+++ advent-of-code-2020/day21.rkt
@@ -1,37 +1,35 @@
 #lang racket
 (require graph threading rackunit)
-
 (struct food (ingredients allergens ing-set inert) #:transparent)
 
 (define (parse-input fname)
+  (define (parse-line line obj)
+    (let* ([ m (regexp-match #px"^([a-z ]+) \\(contains ([a-z, ]+)\\)$" line) ]
+           [ ingredients (list->set (string-split (second m) " ")) ]
+           [ allergens   (string-split (third m) ", ")             ]
+           [ hsh         (food-allergens obj)                      ])
+      (for ([ allergen (in-list allergens) ])
+        (hash-set! hsh allergen (let ([ st (hash-ref hsh allergen #f) ])
+                                  (if st
+                                      (set-intersect st ingredients)
+                                      ingredients))))
+      (struct-copy food obj [ ingredients (cons ingredients (food-ingredients obj)) ])))
   (let* ([ obj           (foldl parse-line (food '() (make-hash) #f #f) (file->lines fname)) ]
          [ ing-set       (apply set-union (food-ingredients obj))                            ]
          [ non-allergens (set-subtract ing-set (apply set-union (hash-values (food-allergens obj)))) ])
     (struct-copy food obj [ ing-set ing-set ][ inert non-allergens ])))
 
-(define (parse-line line obj)
-  (let* ([ m (regexp-match #px"^([a-z ]+) \\(contains ([a-z, ]+)\\)$" line) ]
-         [ ingredients (list->set (string-split (second m) " ")) ]
-         [ allergens   (string-split (third m) ", ")             ]
-         [ hsh         (food-allergens obj)                      ])
-    (for ([ allergen (in-list allergens) ])
-      (hash-set! hsh allergen (let ([ st (hash-ref hsh allergen #f) ])
-                                (if st
-                                    (set-intersect st ingredients)
-                                    ingredients))))
-    (struct-copy food obj [ ingredients (cons ingredients (food-ingredients obj)) ])))
-
 (define (part1 obj)
   (for/sum ([ non (in-set (food-inert obj)) ])
-    (for/sum ([ ing (in-list (food-ingredients obj)) ])
-      (if (set-member? ing non) 1 0))))
+    (count (curryr set-member? non) (food-ingredients obj))))
 
 (define (part2 obj)
   (let ([ hsh (food-allergens obj) ])
     (~> (sort (maximum-bipartite-matching
                (undirected-graph (for*/list ([ allergen (in-list (hash-keys hsh)) ]
                                              [ ing (in-set (hash-ref hsh allergen)) ])
-                                   (list allergen ing)))) string<? #:key second)
+                                   (list allergen ing))))
+              string<? #:key second)
         (map first _)
         (string-join _ ","))))
 
