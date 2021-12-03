9279e09e10f1d5971f713c0b826550eb85f22a38
Brian Adkins <brian@lojic.com>
2021-12-03 17:49:56 -0500

    a compromise...

--- advent-of-code-2021/solutions/day03/day03-string-version-formatted.rkt
+++ advent-of-code-2021/solutions/day03/day03-string-version-formatted.rkt
@@ -2,36 +2,27 @@
 
 (define input (file->lines "day03.txt"))
 
-(define (col lst n)
-  (map (curryr string-ref n) lst))
+(define (col lst n)    (map (curryr string-ref n) lst))
+(define (to-decimal s) (read (open-input-string (format "#b~a" s))))
+(define (flipc c)      (if (char=? c #\1) #\0 #\1))
 
 (define (common lst)
-  (if (>= (count (curry char=? #\1) lst) (/ (length lst) 2))
+  (if (>= (count (curry char=? #\1) lst)
+          (/ (length lst) 2))
       #\1
       #\0))
 
-(define (to-decimal s)
-  (read (open-input-string (format "#b~a" s))))
-
-(define (flipc c)
-  (if (char=? c #\1)
-      #\0
-      #\1))
-
 (define (flip s)
   (build-string (string-length s)
-                (λ (i)
-                  (flipc (string-ref s i)))))
+                (λ (i) (flipc (string-ref s i)))))
 
 (define (gamma lst)
   (build-string (string-length (car lst))
-                (λ (i)
-                  (common (col lst i)))))
+                (λ (i) (common (col lst i)))))
 
 (define (part1 input)
   (let ([ g (gamma input) ])
-    (* (to-decimal g)
-       (to-decimal (flip g)))))
+    (* (to-decimal g) (to-decimal (flip g)))))
 
 (define (part2 input)
   (define (life input oxy)
@@ -48,7 +39,6 @@
                                lst))
                      (add1 bit))))))
 
-  (* (to-decimal (life input #t))
-     (to-decimal (life input #f))))
+  (* (to-decimal (life input #t)) (to-decimal (life input #f))))
 
 (printf "Part1 = ~s, Part2 = ~s\n" (part1 input) (part2 input))
