fc0bf3a47bf821cf65af549296d9c1ebb41cdc3f
Brian Adkins <brian@lojic.com>
2021-12-06 11:01:43 -0500

    Day 6

---
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require "../../advent/advent.rkt" csv-reading)
+
+(define (part n)
+  (let* ([ fish (map string->number
+                     (car (csv->list (file->string "day06.txt")))) ]
+         [ vr (λ (v i)
+                (vector-ref v i)) ]
+         [ v (iterate (λ (v)
+                        (vector (vr v 1) (vr v 2) (vr v 3) (vr v 4) (vr v 5) (vr v 6)
+                                (+ (vr v 0) (vr v 7)) (vr v 8) (vr v 0)))
+                      (foldl (λ (n v)
+                               (vector-set! v n (add1 (vector-ref v n)))
+                               v)
+                             (make-vector 9 0)
+                             fish)
+                      n) ])
+    (for/sum ([ i (in-range 9) ]) (vector-ref v i))))
+
+;; Tests --------------------------------------------------------------------------------------
+
+(module+ test
+  (require rackunit)
+  (check-equal? (part 80) 371379)
+  (check-equal? (part 256) 1674303997472))
