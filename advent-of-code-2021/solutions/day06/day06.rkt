513f116f5b59c4af98ef2fcdd3db25a663f34695
Brian Adkins <brian@lojic.com>
2021-12-06 11:29:05 -0500

    Remove csv dependency

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
-(require "../../advent/advent.rkt" csv-reading)
+(require "../../advent/advent.rkt")
 
 (define (part n)
   (let* ([ fish (map string->number
-                     (car (csv->list (file->string "day06.txt")))) ]
+                     (string-split (string-trim (file->string "day06.txt")) ",")) ]
          [ vr (λ (v i) (vector-ref v i)) ])
     (vector-sum (iterate (λ (v)
                            (vector (vr v 1) (vr v 2) (vr v 3) (vr v 4) (vr v 5) (vr v 6)
