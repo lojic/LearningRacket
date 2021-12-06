1a9f1a05aa43ca15b0fdf4c17fbd023ff93a0c15
Brian Adkins <brian@lojic.com>
2021-12-06 11:42:58 -0500

    simplified

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -3,19 +3,18 @@
 (require "../../advent/advent.rkt")
 
 (define (part n)
-  (let ([ fish (map string->number
-                    (string-split (string-trim (file->string "day06.txt"))
-                                  ",")) ]
-        [ vr (λ (v i) (vector-ref v i)) ])
-    (vector-sum (iterate (λ (v)
-                           (vector (vr v 1) (vr v 2) (vr v 3) (vr v 4) (vr v 5) (vr v 6)
-                                   (+ (vr v 0) (vr v 7)) (vr v 8) (vr v 0)))
-                         (foldl (λ (n v)
-                                  (vector-set! v n (add1 (vr v n)))
-                                  v)
-                                (make-vector 9 0)
-                                fish)
-                         n))))
+  (vector-sum (iterate (λ (v)
+                         (define vr (curry vector-ref v))
+                         (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6)
+                                 (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
+                       (foldl (λ (n v)
+                                (vector-set! v n (add1 (vector-ref v n)))
+                                v)
+                              (make-vector 9 0)
+                              (map string->number
+                                   (string-split (string-trim (file->string "day06.txt"))
+                                                 ",")))
+                       n)))
 
 ;; Tests --------------------------------------------------------------------------------------
 
