f99eeba65ed41abfe5d9d9ef8d9dd43282cf93e0
Brian Adkins <brian@lojic.com>
2021-12-06 12:08:03 -0500

    formatting

--- advent-of-code-2021/solutions/day06/day06-annotated.rkt
+++ advent-of-code-2021/solutions/day06/day06-annotated.rkt
@@ -5,8 +5,10 @@
 (define (part n)
   ;; Sum the vector result of iterating the simulation
   (vector-sum
+
    ;; Iterate a simulation step function n times
    (iterate
+
     ;; The simulation step function.
     (λ (v)
       (define vr (curry vector-ref v))
@@ -14,16 +16,25 @@
       ;; etc. The exception is for the new 6's - these will come from
       ;; *both* 0's and 7's
       (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
+
     ;; The initial argument to the iteration. Obtain a list of
     ;; integers from the input and create an initial vector where each
     ;; slot contains the count of fishes.
-    (foldl (λ (n v)
-             (vector-set! v n (add1 (vector-ref v n))) v)
-           (make-vector 9 0)
-           (~> (file->string "day06.txt")
-               string-trim
-               (string-split ",")
-               (map string->number _)))
+    (foldl
+
+     ;; Fold function - increment the vector slot corresponding to n
+     (λ (n v)
+       (vector-set! v n (add1 (vector-ref v n))) v)
+
+     ;; Initial value - a vector of zeros
+     (make-vector 9 0)
+
+     ;; List of numbers to fold over
+     (~> (file->string "day06.txt")
+         string-trim
+         (string-split ",")
+         (map string->number _)))
+
     ;; The number of times to iterate
     n)))
 
