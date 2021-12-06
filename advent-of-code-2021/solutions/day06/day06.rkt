7b22459cc6fffb33baa74ceeeb884b4b5c4aedd6
Brian Adkins <brian@lojic.com>
2021-12-06 17:40:17 -0500

    One last re-format :)

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -1,15 +1,20 @@
 #lang racket
 
-(require "../../advent/advent.rkt")
+(require "../../advent/advent.rkt" threading)
 
 (define (part n)
-  (vector-sum (iterate (λ (v)
-                         (define vr (curry vector-ref v))
-                         (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
-                       (foldl (λ (n v) (vector-set! v n (add1 (vector-ref v n))) v)
-                              (make-vector 9 0)
-                              (map string->number (string-split (string-trim (file->string "day06.txt")) ",")))
-                       n)))
+  (~> (iterate (λ (v)
+                 (define vr (curry vector-ref v))
+                 (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
+               (foldl (λ (n v)
+                        (vector-set! v n (add1 (vector-ref v n))) v)
+                      (make-vector 9 0)
+                      (~> (file->string "day06.txt")
+                          string-trim
+                          (string-split ",")
+                          (map string->number _)))
+               n)
+      vector-sum))
 
 ;; Tests --------------------------------------------------------------------------------------
 
