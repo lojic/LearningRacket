5df115c269bfae2f8847f4fdb05102f80a448ff8
Brian Adkins <brian@lojic.com>
2021-12-06 21:08:03 -0500

    another refactor :-o

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -2,16 +2,15 @@
 
 (require "../../advent/advent.rkt" threading)
 
-(define (solve n)
+(define input (~> (file->string "day06.txt") string-trim (string-split ",") (map string->number _)))
+
+(define (solve input n)
   (~> (iterate (λ (v)
                  (define vr (curry vector-ref v))
                  (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
                (foldl (λ (n v) (vector-update! v n add1) v)
                       (make-vector 9 0)
-                      (~> (file->string "day06.txt")
-                          string-trim
-                          (string-split ",")
-                          (map string->number _)))
+                      input)
                n)
       vector-sum))
 
@@ -19,5 +18,5 @@
 
 (module+ test
   (require rackunit)
-  (check-equal? (solve 80) 371379)
-  (check-equal? (solve 256) 1674303997472))
+  (check-equal? (solve input 80) 371379)
+  (check-equal? (solve input 256) 1674303997472))
