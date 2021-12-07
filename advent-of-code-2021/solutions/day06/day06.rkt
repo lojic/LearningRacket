42cab021b128c79a439d1bbeda4bd270f2862f60
Brian Adkins <brian@lojic.com>
2021-12-06 21:14:14 -0500

    Last refactor :)

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -5,14 +5,16 @@
 (define input (~> (file->string "day06.txt") string-trim (string-split ",") (map string->number _)))
 
 (define (solve input n)
-  (~> (iterate (λ (v)
-                 (define vr (curry vector-ref v))
-                 (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
-               (foldl (λ (n v) (vector-update! v n add1) v)
-                      (make-vector 9 0)
-                      input)
-               n)
-      vector-sum))
+  (define (initialize-fish input)
+    (foldl (λ (n v) (vector-update! v n add1) v)
+           (make-vector 9 0)
+           input))
+
+  (define (spawn v)
+    (define vr (curry vector-ref v))
+    (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
+
+  (vector-sum (iterate spawn (initialize-fish input) n)))
 
 ;; Tests --------------------------------------------------------------------------------------
 
