a3a39b03dfa7d7b9b941f03b630b19ce81e1e58e
Brian Adkins <brian@lojic.com>
2021-12-07 11:07:59 -0500

    Day 7

---
+++ advent-of-code-2021/solutions/day07/day07.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(require "../../advent/advent.rkt")
+
+(define (sum-fuel cost positions pos)
+  (for/sum ([ n positions ])
+    (cost (abs (- n pos)))))
+
+(define (solve cost positions)
+  (let ([ sorted (sort positions <) ])
+    (for/fold ([ least 1000000000            ])
+              ([ pos   (range (last sorted)) ])
+      (min least (sum-fuel cost sorted pos)))))
+
+(define part1-cost identity)
+
+(define (part2-cost n) (/ (* n (add1 n)) 2))
+
+;; Tests --------------------------------------------------------------------------------------
+
+(module+ test
+  (require rackunit)
+  (check-equal? (solve part1-cost (csv-file->numbers "day07.txt")) 351901)
+  (check-equal? (solve part2-cost (csv-file->numbers "day07.txt")) 101079875))
