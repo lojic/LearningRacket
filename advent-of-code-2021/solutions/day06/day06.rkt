eb2c2cde6bc652d8fa989f271e233a734d7db5f8
Brian Adkins <brian@lojic.com>
2021-12-06 21:29:28 -0500

    rename input to fish

--- advent-of-code-2021/solutions/day06/day06.rkt
+++ advent-of-code-2021/solutions/day06/day06.rkt
@@ -2,23 +2,23 @@
 
 (require "../../advent/advent.rkt" threading)
 
-(define input (~>> (file->string "day06.txt")
+(define fish (~>> (file->string "day06.txt")
                   string-trim
                   (string-split _ ",")
                   (map string->number)
                   (foldl (λ (n v) (vector-update! v n add1) v)
                          (make-vector 9 0))))
 
-(define (solve input n)
+(define (solve fish n)
   (define (spawn v)
     (define vr (curry vector-ref v))
     (vector (vr 1) (vr 2) (vr 3) (vr 4) (vr 5) (vr 6) (+ (vr 0) (vr 7)) (vr 8) (vr 0)))
 
-  (vector-sum (iterate spawn input n)))
+  (vector-sum (iterate spawn fish n)))
 
 ;; Tests --------------------------------------------------------------------------------------
 
 (module+ test
   (require rackunit)
-  (check-equal? (solve input 80) 371379)
-  (check-equal? (solve input 256) 1674303997472))
+  (check-equal? (solve fish 80) 371379)
+  (check-equal? (solve fish 256) 1674303997472))
