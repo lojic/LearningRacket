a57ac390715d66f39ae11449bb8ba93ab8fb0cd5
Brian Adkins <brian@lojic.com>
2021-12-13 18:16:52 -0500

    Port of Day 12 from Hyper Neutrino

---
+++ advent-of-code-2021/solutions/day12/day12-hyper-neutrino.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define edges (make-hash))
+
+(for ([ line (in-lines) ])
+  (match-define (list a b) (string-split line "-"))
+  (hash-set! edges a (cons b (hash-ref edges a '())))
+  (hash-set! edges b (cons a (hash-ref edges b '()))))
+
+(define (count node [ visited (set) ][ doubled #f ])
+  (define (add-small) (if (string=? node (string-downcase node))
+                          (set-add visited node)
+                          visited))
+
+  (cond [ (string=? node "end") 1 ]
+        [ else (for/sum ([ next (hash-ref edges node) ])
+                 (if (not (or (string=? next "start")
+                              (and (set-member? visited next) doubled)))
+                     (count next (add-small) (if (set-member? visited next) #t doubled))
+                     0)) ]))
+
+(displayln (count "start"))
+
+;; Port of https://github.com/hyper-neutrino/advent-of-code/blob/main/2021/day12p2.py
