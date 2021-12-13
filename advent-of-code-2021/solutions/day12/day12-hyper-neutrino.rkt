527e2cf016280873fec239a3b448f11820bf4643
Brian Adkins <brian@lojic.com>
2021-12-13 18:38:15 -0500

    one more line :)

--- advent-of-code-2021/solutions/day12/day12-hyper-neutrino.rkt
+++ advent-of-code-2021/solutions/day12/day12-hyper-neutrino.rkt
@@ -12,8 +12,7 @@
 
   (cond [ (string=? node "end") 1 ]
         [ else (for/sum ([ next (hash-ref edges node) ])
-                 (cond [ (or (string=? next "start")
-                             (and (set-member? visited next) doubled)) 0 ]
+                 (cond [ (or (string=? next "start") (and (set-member? visited next) doubled)) 0 ]
                        [ else (count next (add-small) (if (set-member? visited next) #t doubled)) ])) ]))
 
 (displayln (count "start"))
