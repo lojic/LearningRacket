25f54c41dcbd758f737a7d99d72229655bcc64c2
Brian Adkins <brian@lojic.com>
2021-12-13 19:04:23 -0500

    Remove mutation, and use graph

--- advent-of-code-2021/solutions/day12/day12-hyper-neutrino.rkt
+++ advent-of-code-2021/solutions/day12/day12-hyper-neutrino.rkt
@@ -1,17 +1,15 @@
 #lang racket
+(require graph threading)
 
-(define edges (make-hash))
-
-(for ([ line (in-lines) ])
-  (match-define (list a b) (string-split line "-"))
-  (hash-set! edges a (cons b (hash-ref edges a '())))
-  (hash-set! edges b (cons a (hash-ref edges b '()))))
+(define g (~>> (sequence->list (in-lines))
+               (map (curryr string-split "-"))
+               undirected-graph))
 
 (define (count node [ visited (set) ][ doubled #f ])
   (define (add-small) (if (string=? node (string-downcase node)) (set-add visited node) visited))
 
   (cond [ (string=? node "end") 1 ]
-        [ else (for/sum ([ next (hash-ref edges node) ])
+        [ else (for/sum ([ next (get-neighbors g node) ])
                  (cond [ (string=? next "start")                  0 ]
                        [ (and (set-member? visited next) doubled) 0 ]
                        [ else (count next (add-small) (if (set-member? visited next) #t doubled)) ])) ]))
@@ -19,3 +17,4 @@
 (displayln (count "start"))
 
 ;; Port of https://github.com/hyper-neutrino/advent-of-code/blob/main/2021/day12p2.py
+;; However, I made it completely functional, and simplified some parts.
