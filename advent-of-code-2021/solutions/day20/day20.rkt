69854d084344b295cef5c7e88b91c772db8e4c86
Brian Adkins <brian@lojic.com>
2021-12-20 15:13:17 -0500

    performance

--- advent-of-code-2021/solutions/day20/day20.rkt
+++ advent-of-code-2021/solutions/day20/day20.rkt
@@ -14,10 +14,10 @@
     (or (and (char=? #\# (vector-ref iea 0)) (char=? #\. c) #\#) #\.))
 
   (define (enhance-pixel x y)
-    (~>> (for*/list ([ dy (inclusive-range -1 1) ]
-                     [ dx (inclusive-range -1 1) ])
-           (vec-get img (+ x dx) (+ y dy)))
-         (map (λ (c) (match c [ #\. 0 ][ #\# 1 ])))
+    (~>> (for*/list ([ dy (in-range -1 2) ]
+                     [ dx (in-range -1 2) ])
+           (match (get-pixel img (+ x dx) (+ y dy))
+             [ #\. 0 ][ #\# 1 ]))
          (bool-list->decimal)
          (vector-ref iea)))
 
@@ -27,17 +27,17 @@
                           (image-def img)) ])
     (for* ([ x (in-range dim) ]
            [ y (in-range dim) ])
-      (vec-set! img-out x y (enhance-pixel (sub1 x) (sub1 y))))
+      (set-pixel! img-out x y (enhance-pixel (sub1 x) (sub1 y))))
     (struct-copy image img-out [ def (next-default (image-def img-out)) ])))
 
-(define (vec-get img x y)
+(define (get-pixel img x y)
   (let* ([ dim (image-dim img) ]
          [ end (sub1 dim)      ])
     (if (and (<= 0 x end) (<= 0 y end))
         (vector-ref (image-vec img) (+ (* y dim) x))
         (image-def img))))
 
-(define (vec-set! img x y c)
+(define (set-pixel! img x y c)
   (vector-set! (image-vec img) (+ (* y (image-dim img)) x) c))
 
 (define (parse file-name)
@@ -54,4 +54,4 @@
   (require rackunit)
   (let ([ solver (curry solve "day20.txt") ])
     (check-equal? (solver 2) 5057)
-    (check-equal? (solver 50) 18502)))
+    (check-equal? (time (solver 50)) 18502)))
