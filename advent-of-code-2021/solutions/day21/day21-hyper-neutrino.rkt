f3af1be2b2e2ab33ba653482ef09fad6ec4baafa
Brian Adkins <brian@lojic.com>
2021-12-21 20:55:32 -0500

    better names

--- advent-of-code-2021/solutions/day21/day21-hyper-neutrino.rkt
+++ advent-of-code-2021/solutions/day21/day21-hyper-neutrino.rkt
@@ -3,30 +3,30 @@
 ;; Port of https://github.com/hyper-neutrino/advent-of-code/blob/main/2021/day21p2.py
 (define cache (make-hash))
 
-(define c (for*/list ([ x (in-inclusive-range 1 3) ]
-                      [ y (in-inclusive-range 1 3) ]
-                      [ z (in-inclusive-range 1 3) ])
-            (+ x y z)))
+(define rolls (for*/list ([ x (in-inclusive-range 1 3) ]
+                          [ y (in-inclusive-range 1 3) ]
+                          [ z (in-inclusive-range 1 3) ])
+                (+ x y z)))
 
-(define (u p0 p1 [s0 0] [s1 0])
-  (let* ([ k    (list p0 p1 s0 s1)    ]
-         [ pair (hash-ref cache k #f) ])
+(define (play pos0 pos1 [score0 0] [score1 0])
+  (let* ([ key  (list pos0 pos1 score0 score1) ]
+         [ pair (hash-ref cache key #f)        ])
     (if pair
         pair
-        (let ([ oc0 0 ][ oc1 0 ])
-          (for ([ r (in-list c) ])
-            (let* ([ p0* (+ p0 r) ]
-                   [ p0* (if (> p0* 10) (- p0* 10) p0*) ]
-                   [ s0* (+ s0 p0*) ])
-              (if (>= s0* 21)
-                  (set! oc0 (add1 oc0))
-                  (let* ([ pair (u p1 p0* s1 s0*) ]
-                         [ dy   (car pair)        ]
-                         [ dx   (cdr pair)        ])
-                    (set! oc0 (+ oc0 dx))
-                    (set! oc1 (+ oc1 dy))))))
-          (let ([ pair (cons oc0 oc1) ])
-            (hash-set! cache k pair)
+        (let ([ wins0 0 ][ wins1 0 ])
+          (for ([ roll (in-list rolls) ])
+            (let* ([ pos0* (+ pos0 roll) ]
+                   [ pos0* (if (> pos0* 10) (- pos0* 10) pos0*) ]
+                   [ score0* (+ score0 pos0*) ])
+              (if (>= score0* 21)
+                  (set! wins0 (add1 wins0))
+                  (let* ([ pair         (play pos1 pos0* score1 score0*) ]
+                         [ delta-wins1  (car pair)                       ]
+                         [ delta-wins0  (cdr pair)                       ])
+                    (set! wins0 (+ wins0 delta-wins0))
+                    (set! wins1 (+ wins1 delta-wins1))))))
+          (let ([ pair (cons wins0 wins1) ])
+            (hash-set! cache key pair)
             pair)))))
 
-(time (u 1 3))
+(time (play 1 3))
