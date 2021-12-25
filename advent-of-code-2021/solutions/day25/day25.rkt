c56bf3c8b6daa1cbeb2688019543a009cdf1d99b
Brian Adkins <brian@lojic.com>
2021-12-25 13:23:56 -0500

    Day 25

---
+++ advent-of-code-2021/solutions/day25/day25.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+(define-values (vec width height east south)
+  (let* ([ lines  (file->lines "day25.txt")         ]
+         [ width  (string-length (car lines))       ]
+         [ height (length lines)                    ]
+         [ vec    (make-vector (* width height) #f) ])
+    (for* ([ x (in-range width)  ]
+           [ y (in-range height) ])
+      (vector-set! vec (+ (* y width) x) (match (string-ref (list-ref lines y) x)
+                                           [ #\>  1 ]
+                                           [ #\v +i ]
+                                           [ _   #f ])))
+    (values vec width height 1 +i)))
+
+(define (x+y idx) (values (modulo (real-part idx) width)
+                          (modulo (imag-part idx) height)))
+
+(define (vget idx)      (let-values ([ (x y) (x+y idx) ]) (vector-ref vec (+ (* y width) x))))
+(define (vset! idx val) (let-values ([ (x y) (x+y idx) ]) (vector-set! vec (+ (* y width) x) val)))
+
+(define (movers dir)
+  (for*/fold ([ result '() ])
+             ([ x (in-range width)  ]
+              [ y (in-range height) ])
+    (let* ([ idx (+ x (* y +i)) ]
+           [ cuc (vget idx)     ])
+      (if (and cuc
+               (= cuc dir)
+               (not (vget (+ dir idx))))
+          (cons idx result)
+          result))))
+
+(define (move-all! dir)
+  (for/sum ([ idx (in-list (movers dir)) ])
+    (let ([ dir (vget idx) ])
+      (vset! idx #f)
+      (vset! (+ idx dir) dir))
+    1))
+
+(define (solve [step 1])
+  (if (> (+ (move-all! east) (move-all! south)) 0)
+      (solve (add1 step))
+      step))
+
+(= (time (solve)) 334)
