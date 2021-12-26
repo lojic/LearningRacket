669b0d3964a4ac742c94901e2a7cc8575fb75a23
Brian Adkins <brian@lojic.com>
2021-12-25 21:20:29 -0500

    Use two sets

--- advent-of-code-2021/solutions/day25/day25-hash.rkt
+++ advent-of-code-2021/solutions/day25/day25-hash.rkt
@@ -4,11 +4,12 @@
 ;; unsurprisingly slower. More than 15x slower. I think the grid would
 ;; have to be very sparse before this version would win.
 
-(define-values (hsh width height east south)
-  (let* ([ lines  (file->lines "day25.txt")   ]
-         [ width  (string-length (car lines)) ]
-         [ height (length lines)              ]
-         [ hsh    (make-hash)                 ])
+(define-values (east-set south-set width height east south)
+  (let* ([ lines     (file->lines "day25.txt")   ]
+         [ width     (string-length (car lines)) ]
+         [ height    (length lines)              ]
+         [ east-set  (mutable-set)               ]
+         [ south-set (mutable-set)               ])
     (for* ([ x (in-range width)  ]
            [ y (in-range height) ])
       (let ([ dir (match (string-ref (list-ref lines y) x)
@@ -16,30 +17,31 @@
                     [ #\v +i ]
                     [ _   #f ]) ])
         (when dir
-          (hash-set! hsh (+ x (* y +i)) dir))))
-    (values hsh width height 1 +i)))
+          (if (= dir 1)
+              (set-add! east-set (+ x (* y +i)))
+              (set-add! south-set (+ x (* y +i)))))))
+    (values east-set south-set width height 1 +i)))
 
 (define (inc idx dir)
   (let* ([ x (modulo (+ (real-part idx) (real-part dir)) width) ]
          [ y (modulo (+ (imag-part idx) (imag-part dir)) height) ])
     (+ x (* y +i))))
 
-(define (movers dir)
-  (filter-map (λ (pair)
-                (match-let ([ (cons idx cuc) pair ])
-                  (if (and (= cuc dir)
-                           (not (hash-ref hsh (inc idx dir) #f)))
-                    idx
-                    #f)))
-              (hash->list hsh)))
+(define (movers s dir)
+  (filter-map (λ (idx)
+                (let ([ idx* (inc idx dir) ])
+                  (if (not (or (set-member? east-set idx*)
+                               (set-member? south-set idx*)))
+                      idx
+                      #f)))
+              (set->list s)))
 
 (define (move-all! dir)
-  (for/sum ([ idx (in-list (movers dir)) ])
-    (let ([ dir* (hash-ref hsh idx) ])
-      (when (not (= dir dir*)) (error "oops"))
-      (hash-remove! hsh idx)
-      (hash-set! hsh (inc idx dir) dir))
-    1))
+  (let ([ s (if (= dir east) east-set south-set) ])
+    (for/sum ([ idx (in-list (movers s dir)) ])
+      (set-remove! s idx)
+      (set-add! s (inc idx dir))
+      1)))
 
 (define (solve [step 1])
   (if (zero? (+ (move-all! east)
