018cd56fbda80002f9d0b7ac5826f7afdafe8f26
Brian Adkins <brian@lojic.com>
2022-12-06 14:34:51 -0500

    Add port of Ben's code

---
+++ advent-of-code-2022/day06/day06-bmp-port.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(require "../advent.rkt")
+
+;; A port of Ben Pollack's Nim code:
+;; https://git.sr.ht/~bmp/aoc2022/tree/main/item/6/solver.nim
+
+(define (solve len)
+  (let ([ contents (file->string "./day06.txt") ])
+    (let loop ([ i len ][ letters (mutable-set) ])
+      (for ([ c (substring contents (- i len) i) ])
+        (set-add! letters c))
+      (if (= (set-count letters) len)
+          i
+          (loop (add1 i) (mutable-set))))))
+
+(time
+ (solve 4)
+ (solve 14))
