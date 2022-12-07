461384735dc4f11176440667bb1eb5618b0dd7e9
Brian Adkins <brian@lojic.com>
2022-12-07 17:24:07 -0500

    Convert to purely functional

--- advent-of-code-2022/day07/day07-after-viewing-others.rkt
+++ advent-of-code-2022/day07/day07-after-viewing-others.rkt
@@ -2,30 +2,33 @@
 (require "../advent.rkt")
 
 (define in   (parse-aoc 7 atoms))
-(define hsh  (make-hash))
-(define seen (make-hash))
-(define cwd  #f)
+(define dirs (hash))
+(define seen (hash))
 
-(define (add-file! size [cwd cwd])
-  (hash-set! hsh cwd (+ size (hash-ref hsh cwd 0)))
-  (when (not (null? cwd))
-    (add-file! size (cdr cwd))))
+(define (add-file dirs size cwd)
+  (let ([ dirs (hash-set dirs cwd (+ size (hash-ref dirs cwd 0))) ])
+    (if (null? cwd)
+        dirs
+        (add-file dirs size (cdr cwd)))))
 
-(for ([ lst (in-list in) ])
-  (match lst
-    [ (list "$" _)         (void) ]
-    [ (list "dir" _)       (void) ]
-    [ (list "$" "cd" "..") (set! cwd (drop cwd 1)) ]
-    [ (list "$" "cd" dir)  (if (string=? "/" dir)
-                               (set! cwd '())
-                               (set! cwd (cons dir cwd))) ]
-    [ (list size name)     (let ([ key (cons name cwd) ])
-                             (when (not (hash-has-key? seen key))
-                               (hash-set! seen key #t)
-                               (add-file! size))) ]))
+(define (process dirs seen cwd in)
+  (if (null? in)
+      dirs
+      (let ([ lst (car in) ]
+            [ rem (cdr in) ])
+        (match lst
+          [ (list "$" _)         (process dirs seen cwd rem)          ]
+          [ (list "dir" _)       (process dirs seen cwd rem)          ]
+          [ (list "$" "cd" "..") (process dirs seen (drop cwd 1) rem) ]
+          [ (list "$" "cd" dir)  (if (string=? "/" dir)
+                                     (process dirs seen '() rem)
+                                     (process dirs seen (cons dir cwd) rem)) ]
+          [ (list size name)     (let ([ key (cons name cwd) ])
+                                   (if (hash-has-key? seen key)
+                                       (process dirs seen cwd rem)
+                                       (process (add-file dirs size cwd) (hash-set seen key #t) cwd rem))) ]))))
 
-(define sizes (~> (hash->list hsh) (map cdr _)))
-
-(list-sum (filter (curry > 100001) sizes))                         ; Part 1
-
-(findf (curry < (+ -40000000 (hash-ref hsh '() ))) (sort sizes <)) ; Part 2
+(let* ([ dirs  (process dirs seen #f in)          ]
+       [ sizes (~> (hash->list dirs) (map cdr _)) ])
+  (values (list-sum (filter (curry > 100001) sizes))                            ; Part 1
+          (findf (curry < (+ -40000000 (hash-ref dirs '() ))) (sort sizes <)))) ; Part 2
