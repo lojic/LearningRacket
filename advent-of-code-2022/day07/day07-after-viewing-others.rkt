a3c8a7cc4614e397715eef338bb5558703cd9762
Brian Adkins <brian@lojic.com>
2022-12-07 16:36:29 -0500

    so simple

---
+++ advent-of-code-2022/day07/day07-after-viewing-others.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(require "../advent.rkt")
+
+(define in  (parse-aoc 7 atoms))
+(define hsh (make-hash))
+(define cwd #f)
+
+(define (add-file! size [cwd cwd])
+  (hash-set! hsh cwd (+ size (hash-ref hsh cwd 0)))
+  (when (not (null? cwd))
+    (add-file! size (cdr cwd))))
+
+(for ([ lst (in-list in) ])
+  (match lst
+    [ (list "$" _)         (void) ]
+    [ (list "dir" _)       (void) ]
+    [ (list "$" "cd" "..") (set! cwd (drop cwd 1)) ]
+    [ (list "$" "cd" dir)  (if (string=? "/" dir)
+                               (set! cwd '())
+                               (set! cwd (cons dir cwd))) ]
+    [ (list size _)        (add-file! size) ]))
+
+(define sizes (~> (hash->list hsh) (map cdr _)))
+
+(list-sum (filter (curry > 100001) sizes))                         ; Part 1
+
+(findf (curry < (+ -40000000 (hash-ref hsh '() ))) (sort sizes <)) ; Part 2
