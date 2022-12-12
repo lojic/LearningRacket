f0a1c7c457f6dc0bcd12827cc9db2096db2f3b5c
Brian Adkins <brian@lojic.com>
2022-12-12 15:57:43 -0500

    sqr is in stdlib - thanks Ben

--- advent-of-code-2022/day11/day11.rkt
+++ advent-of-code-2022/day11/day11.rkt
@@ -3,15 +3,14 @@
 
 (struct monkey (div t f op items) #:transparent)
 
-(define monkeys (let ([ square (λ (n) (* n n)) ])
-                  (vector (monkey  5 1 6 (curry * 13) '(52 78 79 63 51 94))
-                          (monkey  7 5 3 (curry + 3)  '(77 94 70 83 53))
-                          (monkey 13 0 6 square       '(98 50 76))
-                          (monkey 11 5 7 (curry + 5)  '(92 91 61 75 99 63 84 69))
-                          (monkey  3 2 0 (curry + 7)  '(51 53 83 52))
-                          (monkey  2 4 7 (curry + 4)  '(76 76))
-                          (monkey 17 1 3 (curry * 19) '(75 59 93 69 76 96 65))
-                          (monkey 19 2 4 (curry + 2)  '(89)))))
+(define monkeys (vector (monkey  5 1 6 (curry * 13) '(52 78 79 63 51 94))
+                        (monkey  7 5 3 (curry + 3)  '(77 94 70 83 53))
+                        (monkey 13 0 6 sqr          '(98 50 76))
+                        (monkey 11 5 7 (curry + 5)  '(92 91 61 75 99 63 84 69))
+                        (monkey  3 2 0 (curry + 7)  '(51 53 83 52))
+                        (monkey  2 4 7 (curry + 4)  '(76 76))
+                        (monkey 17 1 3 (curry * 19) '(75 59 93 69 76 96 65))
+                        (monkey 19 2 4 (curry + 2)  '(89))))
 
 (define N   (vector-length monkeys))
 (define vec (make-vector N 0))
