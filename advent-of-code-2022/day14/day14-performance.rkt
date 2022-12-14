a1e2646714d5f0e95249c379c986408ca41e59e6
Brian Adkins <brian@lojic.com>
2022-12-14 18:27:25 -0500

    More reaonable format

--- advent-of-code-2022/day14/day14-performance.rkt
+++ advent-of-code-2022/day14/day14-performance.rkt
@@ -11,32 +11,39 @@
   (define bottom    (+ 2 (list-max (map imag-part (flatten in)))))
   (define member?   (curry hash-has-key? cave))
   (define add!      (λ (p) (hash-set! cave p #t)))
-  (define set-line! (compose (curry map add!)  coordinates-range))
+  (define set-line! (compose (curry map add!) coordinates-range))
 
-  (define (move-sand! path)
+  (define (move-sand! path) ; Simulate a grain of sand
     (let* ([ point (car path)     ]
            [ d     (+ point  0+i) ]
            [ dl    (+ point -1+i) ]
            [ dr    (+ point  1+i) ])
-      (cond [ (> (imag-part point) bottom)  #f               ]    ; Into the void !
-            [ (not (member? d))  (move-sand! (cons d path))  ]    ; Down
-            [ (not (member? dl)) (move-sand! (cons dl path)) ]    ; Down to left
-            [ (not (member? dr)) (move-sand! (cons dr path)) ]    ; Down to right
-            [ else               (add! point) path           ]))) ; Sleeeep (in voice of Mantis)
+      (cond [ (> (imag-part point)
+                 bottom) #f ]                                  ; Into the void !
+            [ (not (member? d))  (move-sand! (cons d path))  ] ; Down
+            [ (not (member? dl)) (move-sand! (cons dl path)) ] ; Down to left
+            [ (not (member? dr)) (move-sand! (cons dr path)) ] ; Down to right
+            [ else                                             ; Sleeeep (in voice of Mantis)
+              (add! point)
+              path ])))
   ;; ------------------------------------------------------------------------------------------
-  (hash-clear! cave)
+
+  (hash-clear! cave) ; Start with a fresh cave
+
   (for ([ path in ]) ; Add rocks
     (for ([ pair (zipn path (cdr path)) ])
       (set-line! (car pair) (cadr pair))))
-  (when floor?       ; Add floor
+
+  (when floor? ; Add floor
     (set-line! (make-rectangular (- source 200) bottom)
                (make-rectangular (+ source 200) bottom)))
 
   (let loop ([ grains 0 ][ path (list source) ])
     (let ([ path (move-sand! path) ])
-      (cond [ (not path)            grains                          ]
-            [ (= (car path) source) (add1 grains)                   ]
-            [ else                  (loop (add1 grains) (cdr path)) ]))))
+      (cond [ (not path)            grains        ]
+            [ (= (car path) source) (add1 grains) ]
+            [ else
+              (loop (add1 grains) (cdr path)) ]))))
 
 (time (check-equal? (solve)      862))
 (time (check-equal? (solve #t) 28744))
