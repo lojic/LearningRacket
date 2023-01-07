1c1d60de23ab7eded184582499329f87f13be18b
Brian Adkins <brian@lojic.com>
2023-01-06 19:30:07 -0500

    Advent of Code 2022 Day 22 Part 2 - all done

--- advent-of-code-2022/day22/day22.rkt
+++ advent-of-code-2022/day22/day22.rkt
@@ -21,9 +21,21 @@
 (define up   -i)
 (define down +i)
 (define start-tile 51+1i)
+(define tile-size 50)
 
 (define (c->i c) (+ (* (sub1 (imag-part c)) width) (sub1 (real-part c))))
 (define (vget c) (vector-ref vec (c->i c)))
+
+(define (solve next-pos)
+  (let loop ([ pos      start-tile ]
+             [ dir      right      ]
+             [ commands commands   ])
+    (if (null? commands)
+        (+ (* 1000 (imag-part pos))
+           (* 4 (real-part pos))
+           (dir-value dir))
+        (let-values ([ (pos* dir*) (execute-command next-pos (car commands) pos dir) ])
+          (loop pos* dir* (cdr commands))))))
 
 (define (clockwise dir)
   (match dir
@@ -39,19 +51,6 @@
     [ (== left)  down  ]
     [ (== up)    left  ]))
 
-(define (in-bounds? pos)
-  (let ([ col (real-part pos) ]
-        [ row (imag-part pos) ])
-    (and (<= 1 col width)                     ; Horizontal ok
-         (<= 1 row height)                    ; Vertical ok
-         (not (char=? #\space (vget pos)))))) ; Not a space
-
-(define (most-tile-pos pos dir)
-  (let loop ([ pos pos ])
-    (if (in-bounds? pos)
-        (loop (+ pos dir))
-        (- pos dir)))) ; We went to far, backup
-
 (define (dir-value dir)
   (match dir
     [ (== right) 0 ]
@@ -59,38 +58,122 @@
     [ (== left)  2 ]
     [ (== up)    3 ]))
 
-(define (next-pos pos dir)
-  (let ([ pos* (+ pos dir) ])
-    (if (in-bounds? pos*)
-        pos*
-        (most-tile-pos pos (- dir)))))
-
-(define (execute-move pos dir n)
+(define (execute-move next-pos pos dir n)
   (if (zero? n)
-      pos
-      (let ([ pos* (next-pos pos dir) ])
+      (values pos dir)
+      (let-values ([ (pos* dir*) (next-pos pos dir) ])
         (if (char=? #\# (vget pos*))
-            pos
-            (execute-move pos* dir (sub1 n))))))
+            (values pos dir)
+            (execute-move next-pos pos* dir* (sub1 n))))))
 
-(define (execute-command command pos dir)
+(define (execute-command next-pos command pos dir)
   (if (number? command)
-      (let ([ pos* (execute-move pos dir command) ])
-        (values pos* dir))
+      (let-values ([ (pos* dir*) (execute-move next-pos pos dir command) ])
+        (values pos* dir*))
       (values  pos (match command
                      [ "L" (counter-clockwise dir) ]
                      [ "R" (clockwise dir)         ]
                      [ _   (error "what?")         ]))))
 
-(define (part1)
-  (let loop ([ pos      start-tile ]
-             [ dir      right      ]
-             [ commands commands   ])
-    (if (null? commands)
-        (+ (* 1000 (imag-part pos))
-           (* 4 (real-part pos))
-           (dir-value dir))
-        (let-values ([ (pos* dir*) (execute-command (car commands) pos dir) ])
-          (loop pos* dir* (cdr commands))))))
+;; Part 1 specific code -----------------------------------------------------------------------
 
-(time (check-equal? (part1) 136054))
+(define (part1-next-pos pos dir)
+  (define (in-bounds? pos)
+    (let ([ col (real-part pos) ]
+          [ row (imag-part pos) ])
+      (and (<= 1 col width)                     ; Horizontal ok
+           (<= 1 row height)                    ; Vertical ok
+           (not (char=? #\space (vget pos)))))) ; Not a space
+
+  (define (most-tile-pos pos dir)
+    (let loop ([ pos pos ])
+      (if (in-bounds? pos)
+          (loop (+ pos dir))
+          (- pos dir)))) ; We went to far, backup
+
+  (let ([ pos* (+ pos dir) ])
+    (if (in-bounds? pos*)
+        (values pos* dir)
+        (values (most-tile-pos pos (- dir)) dir))))
+
+;; Part 2 specific code -----------------------------------------------------------------------
+
+(struct tile (key origin neighbors))
+
+(define tile-b
+  (tile 'b 51+1i (hash right (cons 'a right) down (cons 'c down) left (cons 'e right) up (cons 'f right))))
+(define tile-a
+  (tile 'a 101+1i (hash right (cons 'd left) down (cons 'c left) left (cons 'b left) up (cons 'f up))))
+(define tile-c
+  (tile 'c 51+51i (hash right (cons 'a up) down (cons 'd down) left (cons 'e down) up (cons 'b up))))
+(define tile-d
+  (tile 'd 51+101i (hash right (cons 'a left) down (cons 'f left) left (cons 'e left) up (cons 'c up))))
+(define tile-e
+  (tile 'e 1+101i (hash right (cons 'd right) down (cons 'f down) left (cons 'b right) up (cons 'c right))))
+(define tile-f
+  (tile 'f 1+151i (hash right (cons 'd up) down (cons 'a down) left (cons 'b down) up (cons 'e up))))
+
+(define tiles (hash 'a tile-a 'b tile-b 'c tile-c 'd tile-d 'e tile-e 'f tile-f))
+
+(define (pos->tile pos)
+  (findf (λ (a-tile)
+           (let* ([ orig (tile-origin a-tile)    ]
+                  [ x1   (real-part orig)        ]
+                  [ y1   (imag-part orig)        ]
+                  [ x2   (+ x1 (sub1 tile-size)) ]
+                  [ y2   (+ y1 (sub1 tile-size)) ])
+             (and (<= x1 (real-part pos) x2)
+                  (<= y1 (imag-part pos) y2))))
+         (hash-values tiles)))
+
+(define (hop a-tile pos dir)
+  (match-let ([ (cons key dir*) (hash-ref (tile-neighbors a-tile) dir) ])
+    (transform (tile-origin a-tile) pos dir (tile-origin (hash-ref tiles key)) dir*)))
+
+(define (transform src-origin src-pos src-dir dst-origin dst-dir)
+  (define len (sub1 tile-size))
+
+  (values (cond [ (= src-dir dst-dir)
+                  (let* ([ pos* (+ src-pos src-dir) ]
+                         [ y    (imag-part pos*)    ])
+                    (cond [ (< y 1)   (+ pos* -100+200i) ] ; Special case transition between A & F
+                          [ (> y 200) (+ pos* 100-200i) ]
+                          [ else      pos*           ])) ]
+                [ (and (= src-dir left) (= dst-dir right))
+                  (+ dst-origin (- (+ src-origin (* len +i)) src-pos)) ]
+                [ (and (= src-dir left) (= dst-dir down))
+                  (+ dst-origin (imag-part (- src-pos src-origin))) ]
+                [ (and (= src-dir right) (= dst-dir left))
+                  (- (+ dst-origin (* len +i) len) (* +i (- (imag-part src-pos) (imag-part src-origin)))) ]
+                [ (and (= src-dir right) (= dst-dir up))
+                  (+ (+ dst-origin (* len +i)) (real-part (- (imag-part src-pos) (imag-part src-origin)))) ]
+                [ (and (= src-dir down) (= dst-dir left))
+                  (+ (+ dst-origin len) (* (- (real-part src-pos) (real-part src-origin)) +i)) ]
+                [ (and (= src-dir up) (= dst-dir right))
+                  (+ dst-origin (* +i (- (real-part src-pos) (real-part src-origin)))) ]
+                [ else (error "what?") ])
+          dst-dir))
+
+(define (leaving-tile? a-tile pos dir)
+  (let* ([ origin (tile-origin a-tile) ]
+         [ ox     (real-part origin)   ]
+         [ oy     (imag-part origin)   ]
+         [ x      (real-part pos)      ]
+         [ y      (imag-part pos)      ]
+         [ len    (sub1 tile-size)     ])
+    (or (and (= dir right) (= x (+ ox len)))
+        (and (= dir down)  (= y (+ oy len)))
+        (and (= dir left)  (= x ox))
+        (and (= dir up)    (= y oy)))))
+
+(define (part2-next-pos pos dir)
+  (let ([ a-tile (pos->tile pos) ])
+    (let-values ([ (pos* dir*) (if (leaving-tile? a-tile pos dir)
+                                   (hop a-tile pos dir)
+                                   (values (+ pos dir) dir)) ])
+      (values pos* dir*))))
+
+;; --------------------------------------------------------------------------------------------
+
+(time (check-equal? (solve part1-next-pos) 136054))
+(time (check-equal? (solve part2-next-pos) 122153))
