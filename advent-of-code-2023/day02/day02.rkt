2a6030e49d5a6b79ecff97d1c9bc832e04be9ce1
Brian Adkins <brian@lojic.com>
2023-12-02 11:21:04 -0500

    AoC Day 2

---
+++ advent-of-code-2023/day02/day02.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require "../advent.rkt" threading)
+
+;; Parse input into the form:
+;; ( (<id> (<n> <color>) (<n> <color) ... ) ... )
+(define in (map (λ (lst)
+                  (cons (cadr lst) (chunk 2 (drop lst 2))))
+                (parse-aoc 2 atoms)))
+
+(define (max-pixel lst [red 0] [green 0] [blue 0])
+  (if (null? lst)
+      (list red green blue)
+      (match (car lst)
+        [ (list n "red")   (max-pixel (cdr lst) (max n red) green blue) ]
+        [ (list n "green") (max-pixel (cdr lst) red (max n green) blue) ]
+        [ (list n "blue")  (max-pixel (cdr lst) red green (max n blue)) ])))
+
+;; Parts --------------------------------------------------------------------------------------
+
+(define (part1)
+  (define max-rgb '(12 13 14))
+  (~> (map (parallel-combine cons car (compose1 max-pixel cdr)) in)
+      (filter (compose1 (curry andmap >= max-rgb) cdr) _)
+      (map car _)
+      (list-sum _)))
+
+(define (part2)
+  (~> (map (compose1 max-pixel cdr) in)
+      (map list-prod _)
+      (list-sum)))
+
+;; Tests --------------------------------------------------------------------------------------
+
+(check-equal? (part1) 2528)
+(check-equal? (part2) 67363)
+
