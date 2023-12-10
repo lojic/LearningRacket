96cc28c92c8f7a82f5f4692b98b2ec39a08910ee
Brian Adkins <brian@lojic.com>
2023-12-09 19:24:28 -0500

    AoC 2023 Day 9

---
+++ advent-of-code-2023/day09/day09.rkt
@@ -0,0 +1,24 @@
+#lang racket
+(require "../advent.rkt")
+
+(define lines (parse-aoc 9 numbers))
+
+(define (compute-deltas lst)
+  (cond [ (null? (cdr lst)) '() ]
+        [ else (cons (- (first lst)
+                        (second lst))
+                     (compute-deltas (cdr lst))) ]))
+
+(define (extrapolate lst)
+  (let loop ([ lst (reverse lst) ][ results '() ])
+    (if (andmap (curry = 0) lst)
+        (list-sum (map car results))
+        (loop (compute-deltas lst) (cons lst results)))))
+
+(define part1 (compose1 list-sum (curry map extrapolate)))
+(define part2 (compose1 part1 (curry map reverse)))
+
+;; Tests --------------------------------------------------------------------------------------
+
+(check-equal? (part1 lines) 1681758908)
+(check-equal? (part2 lines) 803)
