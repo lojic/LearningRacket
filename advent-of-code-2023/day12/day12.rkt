37b792494634bc141d19f0d3d2860949c011b353
Brian Adkins <brian@lojic.com>
2023-12-13 12:10:52 -0500

    AoC 2023 Day 12 Part 2

--- advent-of-code-2023/day12/day12.rkt
+++ advent-of-code-2023/day12/day12.rkt
@@ -1,36 +1,50 @@
 #lang racket
 (require "../advent.rkt")
 
+;; Needed a hint to memoize, and since I already had to "cheat", I
+;; also used some ideas from hyper-neutrino's solution.
+
 (define records (map (match-lambda [ (list springs groups)
-                                     (cons (string->list springs)
-                                           (numbers groups)) ])
-                     (parse-aoc 12 string-split)))
+                                           (cons (string->list springs)
+                                                 (numbers groups)) ])
+                           (parse-aoc 12 string-split)))
 
-(define (invalid? springs groups)
-  ;; placeholder
-  #f)
+(define cache (make-hash))
 
-(define (search input springs groups)
-  (if (null? input)
-      (list (reverse springs))
-      (if (invalid? springs groups)
-          (list '())
-          (let ([ c (car input) ])
-            (if (char=? #\? c)
-                (append (search (cdr input) (cons #\. springs) groups)
-                        (search (cdr input) (cons #\# springs) groups))
-                (search (cdr input) (cons c springs) groups))))))
+(define (search springs groups)
+  (define (helper)
+    (match (car springs)
+      [ #\. (search (cdr springs) groups) ]
+      [ #\# (let ([ group (car groups)     ]
+                  [ len   (length springs) ])
+              (cond [ (> group len)  0 ]
+                    [ (memq #\. (take springs group))  0 ]
+                    [ (= group len)  (if (null? (cdr groups)) 1 0) ]
+                    [ (char=? #\# (list-ref springs group))  0 ]
+                    [ else  (search (drop springs (add1 group)) (cdr groups)) ])) ]
+      [ #\? (+ (search (cdr springs) groups)
+               (search (cons #\# (cdr springs)) groups)) ]))
 
-(define (part1)
-  (for/sum ([ record (in-list records) ])
-    (match-let ([ (cons springs groups) record ])
-      (~> (search springs '() #f)
-          (map (λ (l)
-                 (~> (group-consecutive l)
-                     (filter (λ (lst) (char=? #\# (car lst))) _)
-                     (map length _))) _)
-          (filter (λ (l)
-                    (equal? l groups)) _)
-          length))))
+  (cond [ (null? springs) (if (null? groups) 1 0)     ]
+        [ (null? groups)  (if (memq #\# springs) 0 1) ]
+        [ else            (let* ([ key (list springs groups) ])
+                            (or (hash-ref cache key #f)
+                                (let ([ result (helper) ])
+                                  (hash-set! cache key result)
+                                  result))) ]))
 
-(part1)
+(define (part2 records)
+  (for/list ([ record (in-list records) ])
+    (match-let ([ (cons springs groups) record ]
+                [ q (list #\?) ])
+      (cons (append springs q springs q springs q springs q springs)
+            (append groups groups groups groups groups)))))
+
+(define (solve records)
+  (for/sum ([ rec (in-list records) ])
+    (search (car rec) (cdr rec))))
+
+;; Tests --------------------------------------------------------------------------------------
+
+(check-equal? (solve records) 8419)
+(check-equal? (solve (part2 records)) 160500973317706)
