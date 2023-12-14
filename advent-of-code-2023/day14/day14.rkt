06c14694a31eaafc4b54ac2451789697e56604c0
Brian Adkins <brian@lojic.com>
2023-12-14 15:58:17 -0500

    Cycle detection for part 2

--- advent-of-code-2023/day14/day14.rkt
+++ advent-of-code-2023/day14/day14.rkt
@@ -6,6 +6,16 @@
 (define (east  grid) (~> grid (map reverse _) tilt (map reverse _)))
 (define (south grid) (~> grid transpose (map reverse _) tilt (map reverse _) transpose))
 
+(define (roll grid)
+  (~> (split-rocks grid)
+      (map (λ (l) (sort l char>?)) _)
+      (apply append _)))
+
+(define cycle     (compose1 east south west north))
+(define grid      (map string->list (parse-aoc 14)))
+(define tilt      (curry map roll))
+(define transpose (curry apply map list))
+
 (define ((class t) c)
   (if (char=? #\# t)
       (char=? #\# c)
@@ -23,21 +33,27 @@
       (let-values ([ (h t) (splitf-at lst (class (car lst))) ])
         (cons h (split-rocks t)))))
 
-(define (roll grid)
-  (~> (split-rocks grid)
-      (map (λ (l) (sort l char>?)) _)
-      (apply append _)))
+(define (floyd f val x0) ; Floyd "tortoise & hare" cycle detection algorithm
+  (let*-values ([ (hare) (let loop ([ tortoise (f x0) ][ hare (f (f x0)) ])
+                           (if (= (val tortoise) (val hare))
+                               hare
+                               (loop (f tortoise) (f (f hare))))) ]
+                [ (tortoise mu) (let loop ([ mu 0 ][ tortoise x0 ][ hare hare ])
+                                  (if (= (val tortoise) (val hare))
+                                      (values tortoise mu)
+                                      (loop (add1 mu) (f tortoise) (f hare)))) ])
+    (let loop ([ hare (f tortoise) ][ lam 1 ])
+      (if (= (val tortoise) (val hare))
+          (values lam mu)
+          (loop (f hare) (add1 lam))))))
 
-(define cycle     (compose1 east south west north))
-(define grid      (map string->list (parse-aoc 14)))
-(define tilt      (curry map roll))
-(define transpose (curry apply map list))
-(define part1     (compose1 count-load tilt transpose))
+(define part1 (compose1 count-load tilt transpose))
 
-(define (part2)
-  (count-load (transpose (iterate cycle grid 168))))
+(define (part2 grid n)
+  (let-values ([ (lam mu) (floyd cycle (λ (grid) (count-load (transpose grid))) grid) ])
+    (count-load (transpose (iterate cycle grid (+ mu (modulo (- n mu) lam)))))))
 
 ;; Tests --------------------------------------------------------------------------------------
 
 (check-equal? (part1 grid) 102497)
-(check-equal? (part2) 105008)
+(check-equal? (part2 grid 1000000000) 105008)
