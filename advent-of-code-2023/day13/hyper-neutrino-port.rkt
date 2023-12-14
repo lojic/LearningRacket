f0828a3541a54b7e9011f4dfd118efdb125c50a8
Brian Adkins <brian@lojic.com>
2023-12-13 21:06:58 -0500

    Port of hyper-neutrino's solution

---
+++ advent-of-code-2023/day13/hyper-neutrino-port.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(require "../advent.rkt")
+
+;; Port of hyper-neutrino's solution to Racket
+
+(define (find-mirror grid)
+  (or (for/or ([ r (in-inclusive-range 1 (length grid)) ])
+        (let-values ([ (above below) (split-at grid r) ])
+          (and (= 1 (for/sum ([ pair (zipn (reverse above) below) ])
+                      (for/sum ([ pair (zipn (car pair) (cadr pair)) ])
+                        (if (char=? (car pair) (cadr pair)) 0 1))))
+               r)))
+      0))
+
+(for/sum ([ grid (map (curry map string->list) (parse-aoc 13 string-split #:sep "\n\n")) ])
+  (+ (* (find-mirror grid) 100)
+     (find-mirror (apply map list grid))))
