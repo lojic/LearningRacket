2a0d568a21ea56aadf730fbc2b9a4db604268b7d
Brian Adkins <brian@lojic.com>
2023-12-14 14:16:46 -0500

    some refactoring before cycle detection

--- advent-of-code-2023/day14/day14.rkt
+++ advent-of-code-2023/day14/day14.rkt
@@ -1,49 +1,43 @@
 #lang racket
 (require "../advent.rkt")
 
-(define (north grid) (~> grid transpose roll transpose))
-(define (west grid)  (roll grid))
-(define (east grid)  (~> grid (map reverse _) roll (map reverse _)))
-(define (south grid) (~> grid transpose (map reverse _) roll (map reverse _) transpose))
+(define (west  grid) (~> grid tilt))
+(define (north grid) (~> grid transpose tilt transpose))
+(define (east  grid) (~> grid (map reverse _) tilt (map reverse _)))
+(define (south grid) (~> grid transpose (map reverse _) tilt (map reverse _) transpose))
 
-(define cycle     (compose1 east south west north))
-(define grid      (map string->list (parse-aoc 14)))
-(define transpose (curry apply map list))
-
-(define (roll grid)
-  (define (split-rocks lst)
-    (define (cube? c)  (char=? #\# c))
-    (define (rolls? c) (not (cube? c)))
-    
-    (define (helper lst fun)
-      (if (null? lst)
-          '()
-          (let-values ([ (h t) (splitf-at lst fun) ])
-            (cons h (helper t (if (eq? fun cube?) rolls? cube?))))))
-    
-    (helper lst (if (cube? (car lst)) cube? rolls?)))
-  
-  (map (λ (lst)
-         (~> (split-rocks lst)
-             (map (λ (l) (sort l char>?)) _)
-             (apply append _)))
-       grid))
+(define ((class t) c)
+  (if (char=? #\# t)
+      (char=? #\# c)
+      (not (char=? #\# c))))
 
 (define (count-load grid)
   (for/sum ([ lst (in-list grid) ])
-    (for/sum ([ c    (in-list lst)                          ]
-              [ rows (in-inclusive-range (length lst) 1 -1) ])
+    (for/sum ([ rows (in-inclusive-range (length lst) 1 -1) ]
+              [ c    (in-list lst)                          ])
       (if (char=? c #\O) rows 0))))
 
-(define (part1)
-  (count-load (roll (transpose grid))))
+(define (split-rocks lst)
+  (if (null? lst)
+      '()
+      (let-values ([ (h t) (splitf-at lst (class (car lst))) ])
+        (cons h (split-rocks t)))))
+
+(define (roll grid)
+  (~> (split-rocks grid)
+      (map (λ (l) (sort l char>?)) _)
+      (apply append _)))
+
+(define cycle     (compose1 east south west north))
+(define grid      (map string->list (parse-aoc 14)))
+(define tilt      (curry map roll))
+(define transpose (curry apply map list))
+(define part1     (compose1 count-load tilt transpose))
 
 (define (part2)
   (count-load (transpose (iterate cycle grid 168))))
 
 ;; Tests --------------------------------------------------------------------------------------
 
-(check-equal? (part1) 102497)
+(check-equal? (part1 grid) 102497)
 (check-equal? (part2) 105008)
-
-
