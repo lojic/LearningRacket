9bea9964a66ae0134aebdd694edb536c47c662b6
Brian Adkins <brian@lojic.com>
2023-12-16 10:27:25 -0500

    Purely functional, but still ugly

--- advent-of-code-2023/day16/day16.rkt
+++ advent-of-code-2023/day16/day16.rkt
@@ -5,8 +5,6 @@
   (let ([ lines (parse-aoc 16 string->list) ])
     (values (grid->hash lines) (length (car lines)) (length lines))))
 
-(define seen  (mutable-set))
-
 (define (part2)
   (let ([ configs
           (append
@@ -23,50 +21,51 @@
            (for/list ([ row (in-range height) ])
              (cons (make-rectangular (sub1 width) row) -1))) ])
     (list-max (for/list ([ config (in-list configs) ])
-                (set-clear! seen)
                 (part1 (car config) (cdr config))))))
-  
-(define (beam pos dir)
-  (define (next pos dir) (beam (+ pos dir) dir))
-  
+
+(define (beam pos dir seen)
+  (define (next pos dir seen) (beam (+ pos dir) dir seen))
+
   (let ([ key (cons pos dir) ])
-    (when (not (set-member? seen key))
-      (let ([ c (hash-ref grid pos #f) ])
-        (when c
-          (set-add! seen key)
-          (match (cons c dir)
-            [ (cons #\. _) (next pos dir)  ]
-            [ (cons #\- 1)  (next pos dir) ]
-            [ (cons #\- -1) (next pos dir) ]
-            [ (cons #\| +i) (next pos dir) ]
-            [ (cons #\| -i) (next pos dir) ]
-            
-            [ (cons #\\ 1)  (next pos +i) ]
-            [ (cons #\/ -1) (next pos +i) ]
+    (if (set-member? seen key)
+        seen
+        (let ([ c (hash-ref grid pos #f) ])
+          (if (not c)
+              seen
+              (let ([ seen (set-add seen key) ])
+                (match (cons c dir)
+                  [ (cons #\. _) (next pos dir seen)  ]
+                  [ (cons #\- 1)  (next pos dir seen) ]
+                  [ (cons #\- -1) (next pos dir seen) ]
+                  [ (cons #\| +i) (next pos dir seen) ]
+                  [ (cons #\| -i) (next pos dir seen) ]
 
-            [ (cons #\\ +i) (next pos 1)  ]
-            [ (cons #\/ -i) (next pos 1)  ]
+                  [ (cons #\\ 1)  (next pos +i seen) ]
+                  [ (cons #\/ -1) (next pos +i seen) ]
 
-            [ (cons #\\ -1) (next pos -i) ]
-            [ (cons #\/ 1)  (next pos -i) ]
+                  [ (cons #\\ +i) (next pos 1 seen) ]
+                  [ (cons #\/ -i) (next pos 1 seen) ]
 
-            [ (cons #\\ -i) (next pos -1) ]
-            [ (cons #\/ +i) (next pos -1) ]
-            
-            [ (cons #\- +i) (begin (next pos -1) (next pos 1)) ]
-            [ (cons #\- -i) (begin (next pos -1) (next pos 1)) ]
-            
-            [ (cons #\| -1) (begin (next pos -i) (next pos +i)) ]
-            [ (cons #\| 1)  (begin (next pos -i) (next pos +i)) ]))))))
+                  [ (cons #\\ -1) (next pos -i seen) ]
+                  [ (cons #\/ 1)  (next pos -i seen) ]
+
+                  [ (cons #\\ -i) (next pos -1 seen) ]
+                  [ (cons #\/ +i) (next pos -1 seen) ]
+
+                  [ (cons #\- +i) (next pos -1 (next pos 1 seen)) ]
+                  [ (cons #\- -i) (next pos -1 (next pos 1 seen)) ]
+
+                  [ (cons #\| -1) (next pos -i (next pos +i seen)) ]
+                  [ (cons #\| 1)  (next pos -i (next pos +i seen)) ])))))))
 
 (define (part1 pos dir)
-  (beam pos dir)
 
-  (~> (set->list seen)
+
+  (~> (beam pos dir (set))
+      (set->list _)
       (map car _)
       (remove-duplicates _)
       length))
-
 
 (check-equal? (part1 0 1) 7242)
 (check-equal? (part2) 7572)
