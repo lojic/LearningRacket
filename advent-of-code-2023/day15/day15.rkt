bb421421e73dd80377fb3da60f373c23217d0130
Brian Adkins <brian@lojic.com>
2023-12-15 20:37:32 -0500

    rename

--- advent-of-code-2023/day15/day15.rkt
+++ advent-of-code-2023/day15/day15.rkt
@@ -16,7 +16,7 @@
     (for/sum ([ pair xs ][ slot (in-naturals 1) ])
       (* (add1 (hash-code (car pair))) slot (string->number (cdr pair)))))
 
-  (define (collapse lst)
+  (define (simplify lst)
     (define (next-label label lst)
       (let ([ val (findf (λ (l) (string=? label (car l))) lst) ])
         (values val (remove val lst))))
@@ -24,17 +24,17 @@
     (if (null? lst)
         '()
         (match (car lst)
-          [ (list _) (collapse (cdr lst)) ] ; A - with nothing to delete, ignore
+          [ (list _) (simplify (cdr lst)) ] ; A - with nothing to delete, ignore
           [ (list label focal)
             (let-values ([ (next tail) (next-label label (cdr lst)) ])
               (match next
-                [ #f         (cons (cons label focal) (collapse tail)) ] ; No next label, continue
-                [ (list _)   (collapse tail)                           ] ; Next is -, consume both elements
-                [ (list _ _) (collapse (cons next tail))               ])) ]))) ; Next is =, replace focal length
+                [ #f         (cons (cons label focal) (simplify tail)) ] ; No next label, continue
+                [ (list _)   (simplify tail)                           ] ; Next is -, consume both elements
+                [ (list _ _) (simplify (cons next tail))               ])) ]))) ; Next is =, replace focal length
 
   (~> (map (curry (flip string-split) #px"[=-]") input)
       (group-by (compose1 hash-code car) _)
-      (map (compose1 add-power collapse) _)
+      (map (compose1 add-power simplify) _)
       list-sum))
 
 ;; Tests --------------------------------------------------------------------------------------
