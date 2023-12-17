4a897365b6404b8beb4ec49bbc7178c317807c13
Brian Adkins <brian@lojic.com>
2023-12-16 19:14:40 -0500

    clean up

--- advent-of-code-2023/day16/day16.rkt
+++ advent-of-code-2023/day16/day16.rkt
@@ -5,67 +5,42 @@
   (let ([ lines (parse-aoc 16 string->list) ])
     (values (grid->hash lines) (length (car lines)) (length lines))))
 
-(define (part2)
-  (let ([ configs
-          (append
-           ;; top edge
-           (for/list ([ col (in-range width) ])
-             (cons (make-rectangular col 0) +i))
-           ;; bottom edge
-           (for/list ([ col (in-range width) ])
-             (cons (make-rectangular col (sub1 height)) -i))
-           ;; left edge
-           (for/list ([ row (in-range height) ])
-             (cons (make-rectangular 0 row) 1))
-           ;; right edge
-           (for/list ([ row (in-range height) ])
-             (cons (make-rectangular (sub1 width) row) -1))) ])
-    (list-max (for/list ([ config (in-list configs) ])
-                (part1 (car config) (cdr config))))))
-
 (define (beam pos dir seen)
-  (define (next pos dir seen) (beam (+ pos dir) dir seen))
+  (define (ray c seen key pos dir)
+    (let ([ seen (set-add seen key) ]
+          [ x    (real-part dir)    ]
+          [ y    (imag-part dir)    ]
+          [ next (λ (pos dir seen) (beam (+ pos dir) dir seen)) ])
+      (cond [ (or (char=? c #\.)
+                  (and (char=? c #\-) (= 0 y))
+                  (and (char=? c #\|) (= 0 x))) (next pos dir seen) ]
+            [ (char=? c #\\) (next pos (+ y (* x +i))         seen) ]
+            [ (char=? c #\/) (next pos (+ (- y) (* x -i)) seen) ]
+            [ (char=? c #\-) (next pos -1 (next pos 1  seen)) ]
+            [ (char=? c #\|) (next pos -i (next pos +i seen)) ])))
 
   (let ([ key (cons pos dir) ])
-    (if (set-member? seen key)
-        seen
-        (let ([ c (hash-ref grid pos #f) ])
-          (if (not c)
-              seen
-              (let ([ seen (set-add seen key) ])
-                (match (cons c dir)
-                  [ (cons #\. _) (next pos dir seen)  ]
-                  [ (cons #\- 1)  (next pos dir seen) ]
-                  [ (cons #\- -1) (next pos dir seen) ]
-                  [ (cons #\| +i) (next pos dir seen) ]
-                  [ (cons #\| -i) (next pos dir seen) ]
-
-                  [ (cons #\\ 1)  (next pos +i seen) ]
-                  [ (cons #\/ -1) (next pos +i seen) ]
-
-                  [ (cons #\\ +i) (next pos 1 seen) ]
-                  [ (cons #\/ -i) (next pos 1 seen) ]
-
-                  [ (cons #\\ -1) (next pos -i seen) ]
-                  [ (cons #\/ 1)  (next pos -i seen) ]
-
-                  [ (cons #\\ -i) (next pos -1 seen) ]
-                  [ (cons #\/ +i) (next pos -1 seen) ]
-
-                  [ (cons #\- +i) (next pos -1 (next pos 1 seen)) ]
-                  [ (cons #\- -i) (next pos -1 (next pos 1 seen)) ]
-
-                  [ (cons #\| -1) (next pos -i (next pos +i seen)) ]
-                  [ (cons #\| 1)  (next pos -i (next pos +i seen)) ])))))))
+    (cond [ (set-member? seen key) seen ]
+          [ else (let ([ c (hash-ref grid pos #f) ])
+                   (cond [ (not c) seen ]
+                         [ else (ray c seen key pos dir)]))])))
 
 (define (part1 pos dir)
-
-
   (~> (beam pos dir (set))
       (set->list _)
       (map car _)
       (remove-duplicates _)
       length))
+
+(define (part2)
+  (list-max
+   (map (λ (config) (part1 (car config) (cdr config)))
+        (append (map (λ (col) (cons col                          +i))  (range width))
+                (map (λ (col) (cons (+ col (* (sub1 height) +i)) -i))  (range width))
+                (map (λ (row) (cons (* row +i)                   1))   (range width))
+                (map (λ (row) (cons (+ (sub1 width) (* row +i)) -1))   (range width))))))
+
+;; Tests --------------------------------------------------------------------------------------
 
 (check-equal? (part1 0 1) 7242)
 (check-equal? (part2) 7572)
