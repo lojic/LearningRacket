a8a44bc834b4689226c18ae4745b4fee724d65b1
Brian Adkins <brian@lojic.com>
2023-12-17 21:17:09 -0500

    Use vector instead of hash plus perf tuning

--- advent-of-code-2023/day17/day17.rkt
+++ advent-of-code-2023/day17/day17.rkt
@@ -1,19 +1,23 @@
 #lang racket
 (require "../advent.rkt" data/heap)
 
-(define-values (grid goal)
-  (let ([ lines (parse-aoc 17 string->list) ])
-    (values (grid->hash lines #:col-transform (λ (c) (- (char->integer c) 48)))
-            (make-rectangular (sub1 (length (car lines))) (sub1 (length lines))))))
+(define-values (grid goal width height)
+  (let* ([ lines  (parse-aoc 17 string->list)      ]
+         [ width  (length (car lines))             ]
+         [ height (length lines)                   ]
+         [ grid   (make-vector (* width height) 0) ])
+    (for ([ line (in-list lines) ]
+          [ y  (in-naturals)   ])
+      (for ([ loss (in-list line) ]
+            [ x  (in-naturals)  ])
+        (vector-set! grid (+ (* y width) x) (- (char->integer loss) 48))))
+    (values grid (make-rectangular (sub1 width) (sub1 height)) width height)))
 
 (struct state (pos loss prev history) #:transparent)
 
 (define (goal? n obj)
   (and (= goal (state-pos obj))
        (>= (length (same-dir obj)) n)))
-
-(define (cache-key obj)
-  (string-join (map number->string (cons (state-pos obj) (same-dir obj))) ":"))
 
 (define (same-dir obj [ dir #f ])
   (let ([ history (state-history obj) ])
@@ -25,19 +29,19 @@
   (match-let ([ (state pos loss prev history) obj ])
     (map (λ (n)
            (let* ([ pos*  (+ pos n)                     ]
-                  [ loss* (+ loss (hash-ref grid pos*)) ])
+                  [ loss* (+ loss (vector-ref grid (+ (* (imag-part pos*) width) (real-part pos*)))) ])
              (state pos* loss* pos (cons n history))))
          (filter (λ (n)
                    (let ([ next (+ pos n) ])
-                     (cond [ (= next prev)                   #f ] ; Can't be previous position
-                           [ (not (hash-has-key? grid next)) #f ] ; Must be in grid
+                     (cond [ (= next prev) #f ] ; Can't be previous position
+                           [ (not (and (<= 0 (real-part next) (sub1 width))
+                                       (<= 0 (imag-part next) (sub1 height)))) #f ] ; Must be in grid
                            [ else
                              (let* ([ same (same-dir obj) ]
                                     [ len  (length same)  ])
-                               (cond [ (= max-dir len) (not (= n (car same))) ] ; At most max-dir moves in same dir
-                                     [ (= 0 len)  #t ]                          ; No history, anything is ok
-                                     [ (< len min-dir)  (= n (car same)) ]      ; < min-dir, must be same dir
-                                     [ else  #t ])) ])))
+                               (cond [ (= max-dir len)   (not (= n (car same))) ] ; At most max-dir moves in same dir
+                                     [ (< 0 len min-dir) (= n (car same))       ]  ; < min-dir, must be same dir
+                                     [ else              #t                     ])) ])))
                  '(1 +i -1 -i)))))
 
 (define (solve get-candidates goal?)
@@ -49,7 +53,7 @@
     (let bfs ()
       (let ([ obj (heap-min queue) ])
         (heap-remove-min! queue)
-        (let ([ key (cache-key obj) ])
+        (let ([ key (apply string-append (number->string (state-pos obj)) (map number->string (same-dir obj))) ])
           (cond [ (set-member? seen key) (bfs) ]
                 [ else (set-add! seen key)
                        (let* ([ candidates (get-candidates obj)     ]
