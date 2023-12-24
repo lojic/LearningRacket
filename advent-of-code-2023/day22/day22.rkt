8c20f981cd9afb5a1e4962ac24bc3b751222c4be
Brian Adkins <brian@lojic.com>
2023-12-23 21:01:58 -0500

    forgot some test code

--- advent-of-code-2023/day22/day22.rkt
+++ advent-of-code-2023/day22/day22.rkt
@@ -5,15 +5,15 @@
 
 (define (parse-brick idx lst)
   (match-let ([ (list x1 y1 z1 x2 y2 z2) lst ])
-    (cond [ (not (= z1 z2)) ; Z-axis aligned
-            (brick idx (min z1 z2) (max z1 z2) (list (make-rectangular x1 y1))) ] 
-          [ (= x1 x2)       ; Y-axis aligned
+    (cond [ (not (= z1 z2))
+            (brick idx (min z1 z2) (max z1 z2) (list (+ x1 (* y1 +i)))) ]
+          [ (= x1 x2)
             (brick idx z1 z1 (for/list ([ y (in-inclusive-range (min y1 y2) (max y1 y2)) ])
-                               (make-rectangular x1 y))) ]
-          [ else            ; X-axis aligned
+                               (+ x1 (* y +i)))) ]
+          [ else
             (brick idx z1 z1 (for/list ([ x (in-inclusive-range (min x1 x2) (max x1 x2)) ])
-                               (make-rectangular x y1))) ])))
-  
+                               (+ x (* y1 +i)))) ])))
+
 (define (drop-bricks bricks)
   (define (update-floors hsh b)
     (let ([ z-max (brick-z-max b) ])
@@ -26,14 +26,14 @@
         (reverse result)
         (let* ([ b (car bricks)        ]
                [ z-min (brick-z-min b) ]
-               [ z-max (brick-z-max b) ]
-               [ z (for/fold ([ z 0 ])
-                             ([ point (in-list (brick-points b)) ])
-                     (max z (hash-ref floors point 0))) ]
-               [ delta (sub1 (- z-min z)) ]
+               [ delta (sub1 (- z-min (for/fold ([ z 0 ])
+                                                ([ point (in-list (brick-points b)) ])
+                                        (max z (hash-ref floors point 0))))) ]
                [ b* (if (zero? delta)
                         b
-                        (struct-copy brick b [ z-min (- z-min delta) ][ z-max (- z-max delta) ])) ])
+                        (struct-copy brick b
+                                     [ z-min (- z-min delta) ]
+                                     [ z-max (- (brick-z-max b) delta) ])) ])
           (loop (cdr bricks) (update-floors floors b*) (cons b* result))))))
 
 (define (supported-by b bricks)
@@ -68,38 +68,28 @@
 
 (define (part2)
   (define cache (make-hash))
-  
+
   (define (chain-reaction b bricks removed)
-    (let ([ val (hash-ref cache (cons b removed) #f) ])
-      (if val
-          val
-          (let ([ would-fall (filter (λ (b1)
-                                       (andmap (λ (b2) (set-member? removed b2))
-                                               (supporting b1 bricks)))
-                                     (supported-by b bricks)) ])
-            (cond [ (null? would-fall)
-                    (hash-set! cache (cons b removed) removed)
-                    removed ]
-                  [ else
-                    (let ([ result (for/fold ([ removed (set-union removed (list->set would-fall)) ])
-                                             ([ b* (in-list would-fall) ])
-                                     (set-union removed (chain-reaction b* bricks removed))) ])
-                      (hash-set! cache (cons b removed) result)
-                      result) ])))))
-    
+    (or (hash-ref cache (cons b removed) #f)
+        (let ([ would-fall (filter (λ (b1)
+                                     (andmap (λ (b2) (set-member? removed (brick-idx b2)))
+                                             (supporting b1 bricks)))
+                                   (supported-by b bricks)) ])
+          (cond [ (null? would-fall)
+                  (hash-set! cache (cons b removed) removed)
+                  removed ]
+                [ else
+                  (let ([ result (for/fold ([ removed (set-union removed (list->set (map brick-idx would-fall))) ])
+                                           ([ b* (in-list would-fall) ])
+                                   (set-union removed (chain-reaction b* bricks removed))) ])
+                    (hash-set! cache (cons b removed) result)
+                    result) ]))))
+
   (for/sum ([ b (in-list (sort bricks > #:key brick-z-min)) ])
-    (let ([ n (sub1 (set-count (chain-reaction b bricks (set b)))) ])
-      (printf "Brick: ~a = ~a\n" b n)
+    (let ([ n (sub1 (set-count (chain-reaction b bricks (set (brick-idx b))))) ])
       n)))
 
 ;; Tests --------------------------------------------------------------------------------------
 
 (check-equal? (part1) 482)
-(check-equal? (part2) 103010)
-
-
-          
-  
-                 
-                   
-
+(check-equal? (time (part2)) 103010)
