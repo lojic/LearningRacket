643f16639953480da7b77dfdb59ffd0905589b54
Brian Adkins <brian@lojic.com>
2023-12-26 10:58:08 -0500

    optimize data structures 49 sec -> 4.1 sec

--- advent-of-code-2023/day23/day23.rkt
+++ advent-of-code-2023/day23/day23.rkt
@@ -54,27 +54,56 @@
     (- (list-min (hash-values hsh)))))
 
 (define (part2)
-  (define (dfs g v [ path (set) ][ dist 0 ])
-    (if (= v goal)
-        dist
-        (let ([ neighbors (filter (λ (v*)
-                                    (not (set-member? path v*)))
-                                  (get-neighbors g v)) ])
-          (if (null? neighbors)
-              0
-              (list-max (for/list ([ neighbor (in-list neighbors) ])
-                          (let ([ delta (edge-weight g v neighbor) ])
-                            (dfs g neighbor (set-add path v) (+ dist delta)))))))))
-
   (define (valid-exits pos dir)
     (let ([ dirs (filter (λ (d)
                            (and (not (= d (- dir)))
                                 (memv (hash-ref grid (+ pos d) #\#) '(#\. #\> #\< #\^ #\v))))
                          '(1 +i -1 -i)) ])
       dirs))
+  
+  (define (create-edges g v-index edges) 
+    (let* ([ n   (hash-count v-index)    ]
+           [ vec (make-vector (* n n) 0) ])
+      (for ([ lst (in-list edges) ])
+        (match-let ([ (list v1 v2) lst ])
+        (let ([ i1 (hash-ref v-index v1) ]
+              [ i2 (hash-ref v-index v2) ]
+              [ wt (edge-weight g v1 v2) ])
+          (vector-set! vec (+ i1 (* i2 n)) wt)
+          (vector-set! vec (+ i2 (* i1 n)) wt))))
+      vec))
 
-  (let ([ g (create-graph undirected-graph start identity valid-exits) ])
-    (dfs g 1)))
+  (define (dfs get-neighbors v goal get-weight visited [ dist 0 ])
+    (if (= v goal)
+        dist
+        (let ([ neighbors (filter (λ (v*)
+                                    (not (vector-ref visited v*)))
+                                  (get-neighbors v)) ])
+          (if (null? neighbors)
+              0
+              (list-max (for/list ([ neighbor (in-list neighbors) ])
+                          (let ([ delta (get-weight v neighbor) ])
+                            (vector-set! visited v #t)
+                            (let ([ val (dfs get-neighbors neighbor goal get-weight visited (+ delta dist)) ])
+                              (vector-set! visited v #f)
+                              val))))))))
+  
+  (let* ([ g         (create-graph undirected-graph start identity valid-exits) ]
+         [ vertices  (get-vertices g)                                           ]
+         [ n         (length vertices)                                          ]
+         [ v-index   (make-immutable-hash (enumerate vertices))                 ]
+         [ visited   (make-vector n #f)                                         ]
+         [ neighbors (list->vector (map (λ (v)
+                                          (map (λ (v*)
+                                                 (hash-ref v-index v*))
+                                               (get-neighbors g v)))
+                                        vertices)) ]
+         [ edges         (create-edges g v-index (get-edges g))         ]
+         [ get-neighbors (λ (v) (vector-ref neighbors v))               ]
+         [ get-weight    (λ (v1 v2) (vector-ref edges (+ v1 (* n v2)))) ])
+    (dfs get-neighbors (hash-ref v-index start) (hash-ref v-index goal) get-weight visited)))
+
+;(part2)
 
 ;; Tests --------------------------------------------------------------------------------------
 
