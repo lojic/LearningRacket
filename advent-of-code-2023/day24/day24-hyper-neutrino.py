b5fae3266c7aacaaeaa572f6e7f18d31107010de
Brian Adkins <brian@lojic.com>
2023-12-27 19:01:04 -0500

    formula explanation

--- advent-of-code-2023/day24/day24-hyper-neutrino.py
+++ advent-of-code-2023/day24/day24-hyper-neutrino.py
@@ -2,6 +2,29 @@
 # github as an example of how to use sympy to solve a system of
 # equations.
 
+# For a collision to occur, the position of the rock and the hailstone
+# must be the same at the same instant of time:
+#
+# px + t * vx = xr + t * vxr
+# py + t * vy = yr + t * vyr
+# pz + t * vz = zr + t * vzr
+#
+# So:
+# px + t * vx - xr - t * vxr = 0
+# px + t * (vx - vxr) - xr = 0
+# t * (vx - vxr) = xr - px
+# t = (xr - px) / (vx - vxr)
+#
+# So t must also equal:
+# (yr - py) / (vy - vyr)
+#
+# Setting them equal, we get:
+# (xr - px) * (vy - vyr) = (yr - py) * (vx - vxr)
+# Setting to zero:
+# (xr - px) * (vy - vyr) - (yr - py) * (vx - vxr) = 0  [1]
+# And also for y & z
+# (yr - py) * (vz - vzr) - (zr - pz) * (vy - vyr) = 0  [2]
+
 import sympy
 
 hailstones = [tuple(map(int, line.replace("@", ",").split(","))) for line in open(0)]
@@ -10,9 +33,9 @@ xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vzr")
 
 equations = []
 
-for i, (sx, sy, sz, vx, vy, vz) in enumerate(hailstones):
-    equations.append((xr - sx) * (vy - vyr) - (yr - sy) * (vx - vxr))
-    equations.append((yr - sy) * (vz - vzr) - (zr - sz) * (vy - vyr))
+for i, (px, py, pz, vx, vy, vz) in enumerate(hailstones):
+    equations.append((xr - px) * (vy - vyr) - (yr - py) * (vx - vxr)) # [1]
+    equations.append((yr - py) * (vz - vzr) - (zr - pz) * (vy - vyr)) # [2]
     if i < 2:
         continue
     answers = [soln for soln in sympy.solve(equations) if all(x % 1 == 0 for x in soln.values())]
@@ -20,6 +43,6 @@ for i, (sx, sy, sz, vx, vy, vz) in enumerate(hailstones):
         break
 
 answer = answers[0]
-
+print(answer)
 print(answer[xr] + answer[yr] + answer[zr])
 print(i)
