f8af43fb7029f1ec447dc4127bc3e7f9a04c958e
Brian Adkins <brian@lojic.com>
2025-01-09 12:55:18 -0500

    use cond instead of match

--- advent-of-code-2015/day12.rkt
+++ advent-of-code-2015/day12.rkt
@@ -5,14 +5,14 @@
 (define input (car (parse-aoc 12)))
 
 (define (evaluate obj)
-  (match obj
-    [ (? number?) obj                                          ]
-    [ (? list?)   (for/sum ([ x (in-list obj) ]) (evaluate x)) ]
-    [ (? hash?)   (let ([ vals (hash-values obj) ])
-                    (if (member "red" vals)
-                        0
-                        (for/sum ([ x (in-list vals) ]) (evaluate x)))) ]
-    [ _           0 ]))
+  (cond
+    [ (number? obj) obj ]
+    [ (list? obj)   (for/sum ([ x (in-list obj) ]) (evaluate x)) ]
+    [ (hash? obj)   (let ([ vals (hash-values obj) ])
+                      (if (member "red" vals)
+                          0
+                          (for/sum ([ x (in-list vals) ]) (evaluate x)))) ]
+    [ else          0 ]))
 
 (check-equal? (for/sum ([ i (in-list (numbers input)) ]) i) 111754) ; Part 1
 (check-equal? (evaluate (string->jsexpr input))              65402) ; Part 2
